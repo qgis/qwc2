aa7d8fe6d88986ea803a25d68526b5fc220f971f
Sandro Mani <manisandro@gmail.com>
2016-10-26 20:01:51 +0200

    Add EPSG:25832 proj def

--- js/pages/Main.jsx
+++ js/pages/Main.jsx
@@ -13,7 +13,8 @@ require('./css/mapStyle.css');
 var Proj4js = require('proj4');
 
 
-Proj4js.defs("EPSG:21781","+proj=somerc +lat_0=46.95240555555556 +lon_0=7.439583333333333 +k_0=1 +x_0=600000 +y_0=200000 +ellps=bessel +towgs84=674.4,15.1,405.3,0,0,0,0 +units=m +no_defs");
+Proj4js.defs("EPSG:21781", "+proj=somerc +lat_0=46.95240555555556 +lon_0=7.439583333333333 +k_0=1 +x_0=600000 +y_0=200000 +ellps=bessel +towgs84=674.4,15.1,405.3,0,0,0,0 +units=m +no_defs");
+Proj4js.defs("EPSG:25832", "+proj=utm +zone=32 +ellps=GRS80 +towgs84=0,0,0,0,0,0,0 +units=m +no_defs");
 
 const MapViewer = connect(() => ({}), {
     loadMapConfig: loadMapConfig.bind(null, "mapConfig.json", false)
