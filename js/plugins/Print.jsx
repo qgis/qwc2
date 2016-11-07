822d9a615174126d02b5595306c86ca24920c2ac
Sandro Mani <manisandro@gmail.com>
2016-11-07 19:00:17 +0100

    Finish printing

--- js/plugins/Print.jsx
+++ js/plugins/Print.jsx
@@ -11,6 +11,8 @@ const {connect} = require('react-redux');
 const assign = require('object-assign');
 const Message = require('../../MapStore2/web/client/components/I18N/Message');
 const MapUtils = require('../../MapStore2/web/client/utils/MapUtils');
+const CoordinatesUtils = require('../../MapStore2/web/client/utils/CoordinatesUtils');
+const {changeRotation,zoomToExtent} = require('../../MapStore2/web/client/actions/map');
 const {configurePrintFrame, clearPrintFrame} = require('../../MapStore2/web/client/actions/printframe');
 const {SideBar} = require('../components/SideBar');
 require('./style/Print.css');
@@ -21,7 +23,11 @@ const Print = React.createClass({
         theme: React.PropTypes.object,
         map: React.PropTypes.object,
         configurePrintFrame: React.PropTypes.func,
-        clearPrintFrame: React.PropTypes.func
+        clearPrintFrame: React.PropTypes.func,
+        themeLayerId: React.PropTypes.string,
+        layers: React.PropTypes.array,
+        changeRotation: React.PropTypes.func,
+        zoomToExtent: React.PropTypes.func
     },
     getDefaultProps() {
         return {
@@ -40,17 +46,18 @@ const Print = React.createClass({
             }
             newState["layout"] = layout;
         }
+        if(!this.state.scale && newProps.map) {
+            newState["scale"] = Math.round(MapUtils.getGoogleMercatorScale(newProps.map.zoom + 1));
+        }
         this.setState(newState);
         if(newProps.visible) {
             this.updatePrintArea(newProps.map, newState.scale, newState.layout);
         }
     },
-    onShow() {
-        let mapScale = Math.round(MapUtils.getGoogleMercatorScale(this.props.map.zoom + 1));
-        this.setState({scale: mapScale});
-    },
     onHide() {
         this.props.clearPrintFrame();
+        this.setState({scale: null});
+        this.props.changeRotation(0);
     },
     renderBody() {
         if(!this.props.theme) {
@@ -59,36 +66,74 @@ const Print = React.createClass({
             return (<div role="body"><Message msgId="print.nolayouts" /></div>);
         }
         let currentLayoutname = this.state.layout ? this.state.layout.name : "";
+        let mapName = this.state.layout ? this.state.layout.map.name : "";
+        let themeLayer = this.props.layers.find(layer => layer.id === this.props.themeLayerId);
+        let extent = this.computeCurrentExtent();
+        let formvisibility = 'hidden';
         return (
-            <div role="body">
-                <table className="options-table"><tbody>
-                    <tr>
-                        <td><Message msgId="print.layout" /></td>
-                        <td>
-                            <select onChange={this.changeLayout} value={currentLayoutname}>
-                                {this.props.theme.print.map(item => {
-                                    return (
-                                        <option key={item.name} value={item.name}>{item.name}</option>
-                                    )
-                                })}
-                            </select>
-                        </td>
-                    </tr>
-                    <tr>
-                        <td><Message msgId="print.scale" /></td>
-                        <td><span className="input-frame">1 : <input type="number" value={this.state.scale} onChange={this.changeScale}/></span></td>
-                    </tr>
-                    <tr>
-                        <td><Message msgId="print.resolution" /></td>
-                        <td><span className="input-frame"><input type="number" value={this.state.dpi} onChange={this.changeResolution}/> dpi</span></td>
-                    </tr>
-                </tbody></table>
+            <div role="body" className="scrollable">
+                <form action={this.props.theme.url} method="POST" target="_blank">
+                    <table className="options-table"><tbody>
+                        <tr>
+                            <td><Message msgId="print.layout" /></td>
+                            <td>
+                                <select name="TEMPLATE" onChange={this.changeLayout} value={currentLayoutname}>
+                                    {this.props.theme.print.map(item => {
+                                        return (
+                                            <option key={item.name} value={item.name}>{item.name}</option>
+                                        )
+                                    })}
+                                </select>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td><Message msgId="print.scale" /></td>
+                            <td>
+                                <span className="input-frame">
+                                    <span>1&nbsp;:&nbsp;</span>
+                                    <input name={mapName + ":scale"} type="number" value={this.state.scale} onChange={this.changeScale} min="1"/>
+                                </span>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td><Message msgId="print.resolution" /></td>
+                            <td>
+                                <span className="input-frame">
+                                    <input name="DPI" type="number" value={this.state.dpi} onChange={this.changeResolution} min="50" max="1200"/>
+                                    <span> dpi</span>
+                                </span>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td><Message msgId="print.rotation" /></td>
+                            <td>
+                                <span className="input-frame">
+                                    <input name={mapName + ":rotation"} type="number" value={Math.round(this.props.map.bbox.rotation / Math.PI * 180.)} onChange={this.changeRotation}/>
+                                </span>
+                            </td>
+                        </tr>
+                    </tbody></table>
+                    <div>
+                        <input readOnly="true" name={mapName + ":extent"} type={formvisibility} value={extent} />
+                        <input readOnly="true" name="SERVICE" type={formvisibility} value="WMS" />
+                        <input readOnly="true" name="VERSION" type={formvisibility} value="1.3" />
+                        <input readOnly="true" name="REQUEST" type={formvisibility} value="GetPrint" />
+                        <input readOnly="true" name="FORMAT" type={formvisibility} value="pdf" />
+                        <input readOnly="true" name="TRANSPARENT" type={formvisibility} value="true" />
+                        <input readOnly="true" name="SRS" type={formvisibility} value="EPSG:3857" />
+                        <input readOnly="true" name="LAYERS" type={formvisibility} value={themeLayer ? themeLayer.params.LAYERS : ""} />
+                        <input readOnly="true" name="OPACITIES" type={formvisibility} value={themeLayer ? themeLayer.params.OPACITIES : ""} />
+                    </div>
+                    <div className="button-bar">
+                        <button type="submit">Print</button>
+                    </div>
+                </form>
             </div>
         );
     },
     render() {
         return (
-            <SideBar id="Print" onHide={this.onHide} onShow={this.onShow} width="16em" title="print.paneltitle">
+            <SideBar id="Print" onHide={this.onHide} width="16em" title="print.paneltitle">
                 {this.renderBody()}
             </SideBar>
         );
@@ -105,6 +150,16 @@ const Print = React.createClass({
     changeResolution(ev) {
         this.setState({dpi: ev.target.value});
     },
+    changeRotation(ev) {
+        let angle = parseFloat(ev.target.value);
+        while(angle < 0) {
+            angle += 360;
+        }
+        while(angle >= 360) {
+            angle -= 360;
+        }
+        this.props.changeRotation(angle / 180. * Math.PI);
+    },
     updatePrintArea(map, scale, layout) {
         if(!this.state.layout) {
             return;
@@ -112,19 +167,36 @@ const Print = React.createClass({
         let widthmm = layout.map.width;
         let heightmm = layout.map.height;
         this.props.configurePrintFrame(map.center, scale, widthmm, heightmm);
+    },
+    computeCurrentExtent() {
+        if(!this.props.map || !this.state.layout || !this.state.scale) {
+            return "";
+        }
+        let center = CoordinatesUtils.reproject(this.props.map.center, this.props.map.center.crs, "EPSG:3857");
+        let width = this.state.scale * this.state.layout.map.width / 1000.;
+        let height = this.state.scale * this.state.layout.map.height / 1000.;
+        let x1 = Math.round(center.x - 0.5 * width);
+        let x2 = Math.round(center.x + 0.5 * width);
+        let y1 = Math.round(center.y - 0.5 * height);
+        let y2 = Math.round(center.y + 0.5 * height);
+        return x1 + "," + y1 + "," + x2 + "," + y2;
     }
 });
 
 const selector = (state) => ({
     visible: state.sidebar ? state.sidebar.current === 'Print': false,
     theme: state.theme ? state.theme.current : null,
-    map: state.map ? state.map.present : null
+    map: state.map ? state.map.present : null,
+    themeLayerId: state.theme ? state.theme.currentlayer : "",
+    layers: state.layers ? state.layers.flat : []
 });
 
 module.exports = {
     PrintPlugin: connect(selector, {
         configurePrintFrame: configurePrintFrame,
-        clearPrintFrame: clearPrintFrame
+        clearPrintFrame: clearPrintFrame,
+        changeRotation: changeRotation,
+        zoomToExtent: zoomToExtent
     })(Print),
     reducers: {
         sidebar: require('../reducers/sidebar'),
