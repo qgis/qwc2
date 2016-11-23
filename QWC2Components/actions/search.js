b8e3792706f0cb1dd6009b52a7750dc6c139be85
Sandro Mani <manisandro@gmail.com>
2016-11-23 17:53:57 +0100

    Introduce search provider interface and move search provider implementation to application

--- QWC2Components/actions/search.js
+++ QWC2Components/actions/search.js
@@ -6,9 +6,7 @@
  * LICENSE file in the root directory of this source tree.
  */
 
-const axios = require('axios');
 const {TEXT_SEARCH_RESULTS_LOADED, resultsPurge} = require("../../MapStore2/web/client/actions/search");
-const ConfigUtils = require("../../MapStore2/web/client/utils/ConfigUtils");
 const CoordinatesUtils = require('../../MapStore2/web/client/utils/CoordinatesUtils');
 const UrlParams = require("../utils/UrlParams");
 
@@ -17,8 +15,20 @@ function startSearch(text, searchOptions, searchProviders) {
     return (dispatch) => {
         dispatch(resultsPurge());
         coordinatesSearch(text, searchOptions.displaycrs || "EPSG:4326", dispatch);
-        geoAdminLocationSearch(text, dispatch);
+        if(searchProviders) {
+            Object.keys(searchProviders).map(provider => {
+                searchProviders[provider].onSearch(text, searchOptions, dispatch)
+            });
+        }
     }
+}
+
+function searchMore(moreItem, text, searchProviders) {
+    return (dispatch) => {
+        if(moreItem.provider && searchProviders[moreItem.provider].getMoreResults) {
+            searchProviders[moreItem.provider].getMoreResults(moreItem, text, dispatch);
+        }
+    };
 }
 
 function coordinatesSearch(text, displaycrs, dispatch) {
@@ -74,55 +84,4 @@ function coordinatesSearch(text, displaycrs, dispatch) {
     }
 }
 
-function geoAdminLocationSearch(text, dispatch) {
-    axios.get("http://api3.geo.admin.ch/rest/services/api/SearchServer?searchText="+ encodeURIComponent(text) + "&type=locations&limit=20")
-    .then(response => dispatch(geoAdminLocationSearchResults(response.data)));
-}
-
-function parseItemBBox(bboxstr) {
-    if(bboxstr === undefined) {
-        return null;
-    }
-    let matches = bboxstr.match(/^BOX\s*\(\s*(\d+\.?\d*)\s*(\d+\.?\d*)\s*,\s*(\d+\.?\d*)\s*(\d+\.?\d*)\s*\)$/);
-    if(matches && matches.length < 5) {
-        return null;
-    }
-    let xmin = parseFloat(matches[1]);
-    let ymin = parseFloat(matches[2]);
-    let xmax = parseFloat(matches[3]);
-    let ymax = parseFloat(matches[4]);
-    return CoordinatesUtils.reprojectBbox([xmin, ymin, xmax, ymax], "EPSG:21781", "EPSG:4326");
-}
-
-function geoAdminLocationSearchResults(obj)
-{
-    let resultGroups = {};
-    (obj.results || []).map(entry => {
-        if(resultGroups[entry.attrs.origin] == undefined) {
-            resultGroups[entry.attrs.origin] = {
-                id: entry.attrs.origin,
-                title: "search.geoadmin." + entry.attrs.origin,
-                items: []
-            }
-        }
-        resultGroups[entry.attrs.origin] .items.push({
-            id: entry.id,
-            text: entry.attrs.label,
-            x: entry.attrs.lon,
-            y: entry.attrs.lat,
-            crs: "EPSG:4326",
-            bbox: parseItemBBox(entry.attrs.geom_st_box2d)
-        });
-    });
-    let results = [];
-    for(let key in resultGroups) {
-        results.push(resultGroups[key]);
-    }
-    return {
-        type: TEXT_SEARCH_RESULTS_LOADED,
-        results: results,
-        append: true
-    };
-}
-
-module.exports = {startSearch};
+module.exports = {startSearch, searchMore};
--- QWC2Components/components/Search.jsx
+++ QWC2Components/components/Search.jsx
@@ -11,10 +11,12 @@ const {connect} = require('react-redux');
 const Message = require('../../MapStore2/web/client/components/I18N/Message');
 const SearchBar = require('../../MapStore2/web/client/components/mapcontrols/search/SearchBar');
 const {resultsPurge, resetSearch, searchTextChanged, addMarker} = require("../../MapStore2/web/client/actions/search");
-const {changeMapView} = require('../../MapStore2/web/client/actions/map');
+const {changeMapView} = require('../actions/map');
 const mapUtils = require('../../MapStore2/web/client/utils/MapUtils');
 const CoordinatesUtils = require('../../MapStore2/web/client/utils/CoordinatesUtils');
-const {startSearch} = require("../actions/search");
+const {addLayer, removeLayer} = require('../../MapStore2/web/client/actions/layers');
+const {startSearch,searchMore} = require("../actions/search");
+const IdentifyUtils = require('../utils/IdentifyUtils');
 require('./style/Search.css');
 
 const Search = React.createClass({
@@ -24,12 +26,15 @@ const Search = React.createClass({
         mapConfig: React.PropTypes.object,
         displaycrs: React.PropTypes.string,
         onSearch: React.PropTypes.func,
+        searchMore: React.PropTypes.func,
         onPurgeResults: React.PropTypes.func,
         onSearchReset: React.PropTypes.func,
         onSearchTextChange: React.PropTypes.func,
         panToResult: React.PropTypes.func,
         addMarker: React.PropTypes.func,
-        searchProviders: React.PropTypes.object
+        searchProviders: React.PropTypes.object,
+        addLayer: React.PropTypes.func,
+        removeLayer: React.PropTypes.func
     },
     getDefaultProps() {
         return {
@@ -37,6 +42,9 @@ const Search = React.createClass({
             results: [],
             mapConfig: undefined
         }
+    },
+    getInitialState() {
+        return {currentResult: null}
     },
     renderSearchResults() {
         if(!this.props.results || this.props.results.length === 0) {
@@ -49,14 +57,13 @@ const Search = React.createClass({
         );
     },
     render() {
-        console.log(this.props.searchProviders);
         return (
             <div id="Search">
                 <SearchBar
                     searchText={this.props.searchText}
                     onSearch={this.onSearch}
                     onPurgeResults={this.props.onPurgeResults}
-                    onSearchReset={this.props.onSearchReset}
+                    onSearchReset={this.resetSearch}
                     onSearchTextChange={this.props.onSearchTextChange}
                     delay={500}
                     searchOptions={{displaycrs: this.props.displaycrs}} />
@@ -66,47 +73,79 @@ const Search = React.createClass({
     },
     renderCategory(category) {
         return (
-            <li key={category.key}>
-                <span className="search-results-category-title"><Message msgId={category.title}/></span>
+            <li key={category.id}>
+                <span className="search-results-category-title">{category.title}</span>
                 <ul>{category.items.map(item => this.renderItem(item))}</ul>
             </li>
         )
     },
     renderItem(item) {
+        if(item.more) {
+            return (
+                <li key={item.id} onClick={() => this.props.searchMore(item, this.props.searchText, this.props.searchProviders)}><i><Message msgId="search.more" /></i></li>
+            );
+        }
         return (
-            <li key={item.id} title={item.text} onClick={() => this.itemClicked(item.x, item.y, item.crs, item.bbox)} dangerouslySetInnerHTML={{__html: item.text}}></li>
-        )
+            <li key={item.id} title={item.text} onClick={() => this.itemClicked(item)} dangerouslySetInnerHTML={{__html: item.text}}></li>
+        );
     },
-    itemClicked(x, y, crs, bbox) {
-        this.props.addMarker({lat: y, lng: x});
+    itemClicked(item) {
+        this.props.removeLayer("searchselection");
+        let wgscenterlatlon = CoordinatesUtils.reproject(item, item.crs, "EPSG:4326");
+        let wgsextent = CoordinatesUtils.reprojectBbox(item.bbox, item.crs, "EPSG:4326");
+        this.props.addMarker({lat: wgscenterlatlon.y, lng: wgscenterlatlon.x});
         if(this.props.mapConfig !== undefined) {
-            let newZoom = mapUtils.getZoomForExtent(CoordinatesUtils.reprojectBbox(bbox, crs, this.props.mapConfig.projection), this.props.mapConfig.size, 0, 21, null);
+            let newZoom = mapUtils.getZoomForExtent(CoordinatesUtils.reprojectBbox(item.bbox, item.crs, this.props.mapConfig.projection), this.props.mapConfig.size, 0, 21, null);
             this.props.panToResult(
-                {x, y, crs},
+                {x: wgscenterlatlon.x, y: wgscenterlatlon.y, crs: "EPSG:4326"},
                 newZoom,
                 {
                     bounds: {
-                        minx: bbox[0],
-                        miny: bbox[1],
-                        maxx: bbox[2],
-                        maxy: bbox[3]
+                        minx: wgsextent[0],
+                        miny: wgsextent[1],
+                        maxx: wgsextent[2],
+                        maxy: wgsextent[3]
                     },
-                    crs: crs,
+                    crs: item.crs,
                     rotation: 0
                 },
                 this.props.mapConfig.size,
                 null,
                 this.props.mapConfig.projection);
         }
+        if(item.provider && this.props.searchProviders[item.provider].getResultGeometry) {
+            this.props.searchProviders[item.provider].getResultGeometry(item, this.showFeatureGeometry);
+        }
+        this.setState({currentResult: item});
+    },
+    showFeatureGeometry(item, geometry, crs) {
+        if(item !== this.state.currentResult) {
+            return;
+        }
+        let layer = {
+            id: "searchselection",
+            name: "Search selection",
+            title: "Selection",
+            type: "vector",
+            features: [IdentifyUtils.wktToGeoJSON(geometry)],
+            featuresCrs: crs,
+            visibility: true
+        };
+        this.props.addLayer(layer, true);
     },
     onSearch(text, searchOptions) {
         this.props.onSearch(text, searchOptions, this.props.searchProviders);
+    },
+    resetSearch() {
+        this.setState({currentResult: null});
+        this.props.onSearchReset();
+        this.props.removeLayer("searchselection");
     }
 });
 
 const selector = (state) => ({
     searchText: state.search ? state.search.searchText : "",
-    results: state.search ? state.search.results : [],
+    results: state.search ? state.search.results : null,
     mapConfig: state.map ? state.map.present : undefined,
     displaycrs: state.mousePosition ? state.mousePosition.crs : "EPSG:4326"
 });
@@ -114,10 +153,13 @@ const selector = (state) => ({
 module.exports = {
     Search: connect(selector, {
         onSearch: startSearch,
+        searchMore: searchMore,
         onPurgeResults: resultsPurge,
         onSearchReset: resetSearch,
         onSearchTextChange: searchTextChanged,
         panToResult: changeMapView,
-        addMarker: addMarker
+        addMarker: addMarker,
+        addLayer: addLayer,
+        removeLayer: removeLayer
     })(Search)
 };
