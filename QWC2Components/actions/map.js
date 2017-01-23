cf536d446e10ca911074044af6f8ec168a78d1bf
Sandro Mani <manisandro@gmail.com>
2017-01-23 15:17:26 +0100

    Allow switching between extent and centerAndZoom for URL position format

--- QWC2Components/actions/map.js
+++ QWC2Components/actions/map.js
@@ -9,15 +9,25 @@
 
 const UrlParams = require("../utils/UrlParams");
 const CoordinatesUtils = require("../../MapStore2/web/client/utils/CoordinatesUtils");
+const ConfigUtils = require("../../MapStore2/web/client/utils/ConfigUtils");
+const MapUtils = require("../../MapStore2/web/client/utils/MapUtils");
 
 function changeMapView(center, zoom, bbox, size, mapStateSource, projection) {
     return (dispatch) => {
+        let positionFormat = ConfigUtils.getConfigProp("urlPositionFormat");
         let bounds = CoordinatesUtils.reprojectBbox(bbox.bounds, bbox.crs, "EPSG:4326");
-        let xmin = Math.round(bounds[0] * 100000.) / 100000.;
-        let ymin = Math.round(bounds[1] * 100000.) / 100000.;
-        let xmax = Math.round(bounds[2] * 100000.) / 100000.;
-        let ymax = Math.round(bounds[3] * 100000.) / 100000.;
-        UrlParams.updateParams({e: xmin + ";" + ymin + ";" + xmax + ";" + ymax});
+        if(positionFormat === "centerAndZoom") {
+            let x = Math.round(0.5 * (bounds[0] + bounds[2]) * 100000.) / 100000.;
+            let y = Math.round(0.5 * (bounds[1] + bounds[3]) * 100000.) / 100000.;
+            let scale = MapUtils.getScales(projection)[zoom];
+            UrlParams.updateParams({c: x + ";" + y, s: scale});
+        } else {
+            let xmin = Math.round(bounds[0] * 100000.) / 100000.;
+            let ymin = Math.round(bounds[1] * 100000.) / 100000.;
+            let xmax = Math.round(bounds[2] * 100000.) / 100000.;
+            let ymax = Math.round(bounds[3] * 100000.) / 100000.;
+            UrlParams.updateParams({e: xmin + ";" + ymin + ";" + xmax + ";" + ymax});
+        }
         dispatch(require('../../MapStore2/web/client/actions/map').changeMapView(center, zoom, bbox, size, mapStateSource, projection));
     };
 }
