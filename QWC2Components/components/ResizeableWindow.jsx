8fd344931cacbc1728d5c078b12cd9a7772cc4fc
Sandro Mani <manisandro@gmail.com>
2017-03-13 11:47:30 +0100

    Ensure initial ResizeableWindow position is a round number

--- QWC2Components/components/ResizeableWindow.jsx
+++ QWC2Components/components/ResizeableWindow.jsx
@@ -47,8 +47,8 @@ const ResizeableWindow = React.createClass({
     },
     render() {
         let initial = {
-            x: this.props.initialX || 0.5 * (window.innerWidth - this.props.initialWidth),
-            y: this.props.initialY || 0.5 * (window.innerHeight - this.props.initialHeight),
+            x: this.props.initialX || Math.round(0.5 * (window.innerWidth - this.props.initialWidth)),
+            y: this.props.initialY || Math.round(0.5 * (window.innerHeight - this.props.initialHeight)),
             width: 0.8 * this.props.initialWidth,
             height: 0.8 * this.props.initialHeight
         };
