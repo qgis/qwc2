3cd73ac67fbc1dfc591dbfb6963123442a72e725
mohammadamin46bd <mohammad.amin@kristianstad.se>
2017-07-13 15:52:54 +0200

    Updated

--- QWC2Components/actions/config.js
+++ QWC2Components/actions/config.js
@@ -14,8 +14,7 @@ const UrlParams = require("../utils/UrlParams");
 const assign = require('object-assign');
 const axios = require('axios');
 
-function restoreMapConfig(dispatch, params, mapDefaultConfig) {
-
+function restoreMapConfig(dispatch, params) {
     let mapConfig = {
         map: {
             center: {
@@ -27,13 +26,6 @@ function restoreMapConfig(dispatch, params, mapDefaultConfig) {
             layers: []
         }
     };
-
-    if(mapDefaultConfig && mapDefaultConfig.center){
-     mapConfig.map.center.x = mapDefaultConfig.center.x,
-     mapConfig.map.center.y = mapDefaultConfig.center.y,
-     mapConfig.map.center.crs = mapDefaultConfig.center.crs
-    }
-    
 
     // Set initial extent, pos and scale parameter
     UrlParams.updateParams({ie: params.e, e: undefined});
@@ -53,7 +45,7 @@ function loadMapConfig() {
     return (dispatch) => {
 
         var params = UrlParams.getParams();
-        var mapDefaultConfig = ConfigUtils.getMapDefaultConfiguration();
+
         if(params.k) {
             axios.get(ConfigUtils.getConfigProp("qwc2serverUrl") + "/resolvepermalink?key=" + params.k)
             .then(response => {
@@ -61,10 +53,10 @@ function loadMapConfig() {
                     assign(params, response.data.query);
                     UrlParams.updateParams(params);
                 }
-                restoreMapConfig(dispatch, params, mapDefaultConfig);
+                restoreMapConfig(dispatch, params);
             });
         } else {
-            restoreMapConfig(dispatch, params, mapDefaultConfig);
+            restoreMapConfig(dispatch, params);
         }
     };
 }
