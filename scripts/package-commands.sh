cad4ac1c09f5889a719067fc2485be03742b5161
Sandro Mani <manisandro@gmail.com>
2017-08-31 11:24:51 +0200

    Add set -e to package-commands.sh

--- scripts/package-commands.sh
+++ scripts/package-commands.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
+set -e
 
 if [ "$1" == "build" ]; then
-    npm run clean && \
-    mkdir -p ./dist && \
+    npm run clean
+    mkdir -p ./dist
     NODE_ENV='production' webpack
     exit 0
 fi
@@ -12,13 +13,13 @@ if [ "$1" == "prod" ]; then
     else
       config="config.json"
     fi
-    npm run build && \
-    npm run themesconfig && \
-    rm -rf prod && \
-    mkdir -p ./prod/translations \
-    && cp -a ./dist ./index.html ./assets ./themes.json ./prod \
-    && cp -a  $config ./prod/config.json \
-    && cp -a ./translations/data.* ./prod/translations
+    npm run build
+    npm run themesconfig
+    rm -rf prod
+    mkdir -p ./prod/translations
+    cp -a ./dist ./index.html ./assets ./themes.json ./prod
+    cp -a  $config ./prod/config.json
+    cp -a ./translations/data.* ./prod/translations
     exit $?
 fi
 if [ "$1" == "analyze" ]; then
@@ -30,13 +31,13 @@ if [ "$1" == "release" ]; then
     version=$(grep -oP '"version":\s*"\K(.*)(?=")' package.json)
     rm -f ${name}-${version}_appbundle.zip ${name}-${version}_source.zip
 
-    npm run prod && \
-    ln -s prod ${name}-${version}_appbundle && \
+    npm run prod
+    ln -s prod ${name}-${version}_appbundle
     zip -r ${name}-${version}_appbundle.zip ${name}-${version}_appbundle
     rm ${name}-${version}_appbundle
 
     (
-    git ls-files && \
+    git ls-files
     git submodule foreach --recursive --quiet 'git ls-files --with-tree="$sha1" | sed "s|^|$toplevel/$path/|"' | sed "s|^$PWD/||g"
     ) | grep -v '.gitignore' | grep -v '.gitmodules' | zip -@ ${name}-${version}_source.zip
 
