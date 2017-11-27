7393f94b1d1180b5d5ed1f5358728a5f7122afc2
Sandro Mani <manisandro@gmail.com>
2017-11-27 11:31:42 +0100

    Fix broken debug mode

--- MapStore2Components/utils/DebugUtils.js
+++ MapStore2Components/utils/DebugUtils.js
@@ -5,10 +5,13 @@
  * This source code is licensed under the BSD-style license found in the
  * LICENSE file in the root directory of this source tree.
  */
-var url = require('url');
-
-var {createStore, compose, applyMiddleware} = require('redux');
-var thunkMiddleware = require('redux-thunk').default;
+const url = require('url');
+const {createStore, compose, applyMiddleware} = require('redux');
+const thunkMiddleware = require('redux-thunk').default;
+const logger = require('redux-logger').default;
+const immutable = require('redux-immutable-state-invariant').default;
+const {persistState} = require('redux-devtools');
+const DevTools = require('../components/development/DevTools');
 
 
 const urlQuery = url.parse(window.location.href, true).query;
@@ -25,16 +28,12 @@ var DebugUtils = {
     createDebugStore: function(reducer, initialState, userMiddlewares, enhancer) {
         let finalCreateStore;
         if (__DEVTOOLS__ && urlQuery.debug) {
-            let logger = require('redux-logger')();
-            let immutable = require('redux-immutable-state-invariant')();
-            let middlewares = (userMiddlewares || []).concat([immutable, thunkMiddleware, logger]);
-            const {persistState} = require('redux-devtools');
-            const DevTools = require('../components/development/DevTools');
+            let middlewares = (userMiddlewares || []).concat([immutable(), thunkMiddleware, logger]);
             finalCreateStore = compose(
-              applyMiddleware.apply(null, middlewares),
-            window.devToolsExtension ? window.devToolsExtension() : DevTools.instrument(),
-              persistState(window.location.href.match(/[?&]debug_session=([^&]+)\b/))
-          )(createStore);
+                applyMiddleware.apply(null, middlewares),
+                window.devToolsExtension ? window.devToolsExtension() : DevTools.instrument(),
+                persistState(window.location.href.match(/[?&]debug_session=([^&]+)\b/))
+            )(createStore);
         } else {
             let middlewares = (userMiddlewares || []).concat([thunkMiddleware]);
             finalCreateStore = applyMiddleware.apply(null, middlewares)(createStore);
