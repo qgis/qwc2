1be372748ff757d84c5baf345d431ba896bcc975
Sandro Mani <manisandro@gmail.com>
2017-12-12 15:39:47 +0100

    Add Home button

---
+++ QWC2Components/plugins/HomeButton.jsx
@@ -0,0 +1,49 @@
+/**
+ * Copyright 2017, Sourcepole AG.
+ * All rights reserved.
+ *
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+const React = require('react');
+const PropTypes = require('prop-types');
+const {connect} = require('react-redux');
+const {Button, Glyphicon} = require('react-bootstrap');
+const {zoomToExtent} = require('../../MapStore2Components/actions/map');
+require('./style/Buttons.css');
+
+class HomeButton extends React.Component {
+    static propTypes = {
+        currentTheme: PropTypes.object,
+        position: PropTypes.number,
+        zoomToExtent: PropTypes.func
+    }
+    static defaultProps = {
+        position: 5
+    }
+    render() {
+        return (
+            <Button id="HomeButton" onClick={this.resetExtent} style={{bottom: (5 + 4 * this.props.position) + 'em'}}>
+                <Glyphicon glyph="home"/>
+            </Button>
+        );
+    }
+    resetExtent = () => {
+        if(this.props.currentTheme) {
+            let bbox = this.props.currentTheme.initialBbox;
+            this.props.zoomToExtent(bbox.bounds, bbox.crs);
+        }
+    }
+};
+
+module.exports = {
+    HomeButtonPlugin: connect((state) => ({
+        currentTheme: state.theme ? state.theme.current : null
+    }), {
+        zoomToExtent: zoomToExtent
+    })(HomeButton),
+    reducers: {
+        map: require("../../MapStore2Components/reducers/map")
+    }
+};
