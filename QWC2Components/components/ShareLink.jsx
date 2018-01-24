b397a4febd376846a91890ca8b8a6ce1f9267a42
Sandro Mani <manisandro@gmail.com>
2018-01-24 18:48:46 +0100

    Create separate CopyButton component

--- QWC2Components/components/ShareLink.jsx
+++ QWC2Components/components/ShareLink.jsx
@@ -11,33 +11,20 @@ const PropTypes = require('prop-types');
 const CopyToClipboard = require('react-copy-to-clipboard');
 const {Glyphicon, Tooltip, OverlayTrigger} = require('react-bootstrap');
 const Message = require('../../MapStore2Components/components/I18N/Message');
+const CopyButton = require('./widgets/CopyButton');
 require('./style/ShareLink.css');
 
 class ShareLink extends React.Component {
     static propTypes = {
         shareUrl: PropTypes.string
     }
-    state = {
-        copied: false
-    }
     render() {
-        const tooltip = (
-            <Tooltip placement="bottom" className="in" id="tooltip-bottom">
-                {<Message msgId={this.state.copied ? "share.msgCopiedUrl" : "share.msgToCopyUrl"}/>}
-            </Tooltip>
-        );
         return (
             <div className="share-link">
                   <h4><Message msgId="share.directLinkTitle"/></h4>
                   <div className="share-link-frame">
                       <input onFocus={ev => ev.target.select()} type="text" value={this.props.shareUrl} readOnly/>
-                      <OverlayTrigger placement="bottom" overlay={tooltip}>
-                          <CopyToClipboard text={this.props.shareUrl} onCopy={ () => this.setState({copied: true}) } >
-                              <span className="share-link-button" onMouseLeave={() => {this.setState({copied: false}); }} >
-                                  <Glyphicon glyph="copy"/>
-                              </span>
-                          </CopyToClipboard>
-                      </OverlayTrigger>
+                      <CopyButton text={this.props.shareUrl} buttonClass="share-link-button" tooltipAlign="right"/>
                   </div>
             </div>
         );
