d171166c7871028249a4db33afdf7a04ab86e27c
Sandro Mani <manisandro@gmail.com>
2018-03-29 15:27:37 +0200

    Ensure legend tooltip does not overflow browser window area

--- QWC2Components/plugins/LayerTree.jsx
+++ QWC2Components/plugins/LayerTree.jsx
@@ -242,7 +242,9 @@ class LayerTree extends React.Component {
         if(this.state.legendTooltip) {
             let style = {
                 left: this.state.legendTooltip.x,
-                top: this.state.legendTooltip.y
+                top: this.state.legendTooltip.y,
+                maxWidth: (window.innerWidth - this.state.legendTooltip.x - 5),
+                maxHeight: (window.innerHeight - this.state.legendTooltip.y - 5)
             };
             legendTooltip = (
                 <img className="layertree-item-legend-tooltip" style={style} src={this.state.legendTooltip.img} onTouchStart={this.hideLegendTooltip}></img>
