7bb9a4ab033b6a01e4429ad23b1efc1ea7031fac
Sandro Mani <manisandro@gmail.com>
2018-06-21 14:43:28 +0200

    Add makeIconkit.js script

---
+++ scripts/makeIconkit.js
@@ -0,0 +1,24 @@
+const webfontsGenerator = require('webfonts-generator');
+const glob = require('glob');
+const mkdirp = require('mkdirp');
+
+mkdirp.sync('icons/build');
+
+webfontsGenerator({
+  files: glob.sync("icons/*.svg"),
+  dest: 'icons/build',
+  fontName: 'qwc2-icons',
+  templateOptions: {
+      classPrefix: 'icon-',
+      baseSelector: '.icon'
+  },
+  types: ['woff2'],
+  fontHeight: 1000
+
+}, function(error) {
+  if (error) {
+    console.log('Fail!', error);
+  } else {
+    console.log('Done!');
+  }
+})
