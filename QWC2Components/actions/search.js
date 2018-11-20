c3fd0532afb68aa3288ebc0b4a73bf1a1eb7104f
Mathias Walker <mwa@sourcepole.ch>
2018-11-20 11:25:40 +0100

    Fix exception when selecting more search results

--- QWC2Components/actions/search.js
+++ QWC2Components/actions/search.js
@@ -48,7 +48,7 @@ function searchMore(moreItem, text, providers) {
             dispatch({
                 type: SEARCH_SET_REQUEST,
                 id: reqId,
-                providers: moreItem.provider
+                providers: [moreItem.provider]
             });
             providers[moreItem.provider].getMoreResults(moreItem, text, reqId, dispatch);
         }
