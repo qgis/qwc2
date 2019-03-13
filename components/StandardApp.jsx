a2f4cfa400d3d47350cd14d5574de8e4f4e45439
Sandro Mani <manisandro@gmail.com>
2019-03-13 17:10:57 +0100

    Re-add babel-polyfill for IE

--- components/StandardApp.jsx
+++ components/StandardApp.jsx
@@ -9,6 +9,9 @@ const React = require('react');
 const PropTypes = require('prop-types');
 const {Provider, connect} = require('react-redux');
 
+// Needed for IE11 to avoid 'Promise not defined' error in axios
+require("babel-polyfill");
+
 // Avoid Intl is not defined (Intl needed by react-intl further on)
 if (!global.Intl) {
    require('intl')
