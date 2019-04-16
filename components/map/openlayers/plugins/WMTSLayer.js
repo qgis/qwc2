ce096ad674cf152c33694736c2694e96c33499cb
Éric Lemoine <eric.lemoine@oslandia.com>
2019-04-16 17:47:08 +0200

    Remove the unused wmtsToOpenlayersOptions function

--- components/map/openlayers/plugins/WMTSLayer.js
+++ components/map/openlayers/plugins/WMTSLayer.js
@@ -10,20 +10,6 @@ const ol = require('openlayers');
 const assign = require('object-assign');
 const CoordinatesUtils = require('../../../../utils/CoordinatesUtils');
 
-function wmtsToOpenlayersOptions(options) {
-    // NOTE: can we use opacity to manage visibility?
-    return assign({}, options.baseParams, {
-        LAYERS: options.name,
-        STYLES: options.style || "",
-        FORMAT: options.format || 'image/png',
-        TRANSPARENT: options.transparent !== undefined ? options.transparent : true,
-        SRS: CoordinatesUtils.normalizeSRS(options.srs || 'EPSG:3857', options.allowedSRS),
-        CRS: CoordinatesUtils.normalizeSRS(options.srs || 'EPSG:3857', options.allowedSRS),
-        TILED: options.tiled || false,
-        VERSION: options.version || "1.3.0"
-    }, options.params || {});
-}
-
 function getWMSURLs( urls ) {
     return urls.map((url) => url.split("\?")[0]);
 }
@@ -31,7 +17,6 @@ function getWMSURLs( urls ) {
 let WMTSLayer = {
     create: (options) => {
         const urls = getWMSURLs(Array.isArray(options.url) ? options.url : [options.url]);
-        const queryParameters = wmtsToOpenlayersOptions(options) || {};
         var projection = ol.proj.get(options.projection);
         let resolutions = options.resolutions;
         let matrixIds = new Array(options.resolutions.length);
