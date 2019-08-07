b21a0222a9d2e0929583d539be1a03de8d38e14b
Sandro Mani <manisandro@gmail.com>
2019-08-07 22:14:15 +0200

    Move common application icons to qwc2 submodule, adjust makeIconkit.js accordingly

--- scripts/makeIconkit.js
+++ scripts/makeIconkit.js
@@ -1,11 +1,42 @@
 const webfontsGenerator = require('webfonts-generator');
 const glob = require('glob');
 const mkdirp = require('mkdirp');
+const fs = require('fs');
+const path = require('path');
+
+const readJSON = (path) => {
+    try {
+        return JSON.parse(fs.readFileSync(process.cwd() + path, "utf8"));
+    } catch(e) {
+        return {}
+    }
+}
+
+let workspaces = readJSON('/package.json').workspaces || [];
+
+let icons = [];
+for(let workspace of workspaces) {
+    icons = icons.concat(glob.sync(workspace + "/icons/*.svg"));
+}
+icons = icons.concat(glob.sync("icons/*.svg"));
+
+// Filter duplicate icons (user icons can override submodule icons, hence reverse)
+icons.reverse();
+let uniqueIcons = new Set();
+icons = icons.filter(icon => {
+   let iconName = path.basename(icon);
+   if(uniqueIcons.has(iconName)) {
+       console.log("* " + icon + " was overriden");
+       return false;
+   }
+   uniqueIcons.add(iconName);
+   return true;
+});
 
 mkdirp.sync('icons/build');
 
 webfontsGenerator({
-  files: glob.sync("icons/*.svg"),
+  files: icons,
   dest: 'icons/build',
   fontName: 'qwc2-icons',
   templateOptions: {
