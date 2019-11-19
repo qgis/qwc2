c1c63638d33d9eadf555acdc3926e9788297b62f
Sandro Mani <manisandro@gmail.com>
2019-11-19 18:13:34 +0100

    Fix broken maptips

--- plugins/MapTip.jsx
+++ plugins/MapTip.jsx
@@ -143,7 +143,7 @@ class MapTip extends React.Component {
 
 
 const selector = (state) => ({
-    mapTipsEnabled: state.map && state.map.maptips && state.identify.enabled,
+    mapTipsEnabled: state.map && state.map.maptips && state.identify.tool,
     theme: state.theme && state.theme.current || {},
     layers: state.layers && state.layers.flat ? state.layers.flat : null,
     mousepos: state.mousePosition ? state.mousePosition.position : undefined,
