5b09b3a8800867193a72cff05e3d18b8dd37cc81
Sandro Mani <manisandro@gmail.com>
2020-05-06 17:51:31 +0200

    Accept message IDs as icon title

--- components/Icon.jsx
+++ components/Icon.jsx
@@ -10,6 +10,7 @@ const React = require('react');
 const PropTypes = require('prop-types');
 const classnames = require('classnames');
 const ConfigUtils = require('../utils/ConfigUtils');
+const LocaleUtils = require('../utils/LocaleUtils');
 require('./style/Icon.css');
 
 class Icon extends React.Component {
@@ -17,12 +18,16 @@ class Icon extends React.Component {
         icon: PropTypes.string,
         className: PropTypes.string,
         title: PropTypes.string,
+        titlemsgid: PropTypes.string,
         onClick: PropTypes.func,
         size: PropTypes.string
     }
     static defaultProps = {
         className: "",
         title: ""
+    }
+    static contextTypes = {
+        messages: PropTypes.object
     }
     render() {
         let classes = classnames({
@@ -32,17 +37,21 @@ class Icon extends React.Component {
             [this.props.className]: !!this.props.className,
             "icon_clickable": !!this.props.onClick
         });
+        let title = this.props.title;
+        if(this.props.titlemsgid) {
+            title = LocaleUtils.getMessageById(this.context.messages, this.props.titlemsgid);
+        }
         if(this.props.icon.startsWith(":/")) {
             let assetsPath = ConfigUtils.getConfigProp("assetsPath");
             let src = assetsPath + this.props.icon.substr(1);
             return (
-                <img src={src} alt={this.props.title} title={this.props.title}
+                <img src={src} alt={title} title={title}
                     className={classes} onClick={this.props.onClick}
                 />
             );
         } else {
             return (
-                <span title={this.props.title}
+                <span title={title}
                     className={classes} onClick={this.props.onClick}
                 ></span>
             );
