82c42ff4d0df3fabef4af5ad8b0e1b53b90deeae
Lucian Mandoc <mandoclucian@yahoo.com>
2020-10-23 13:21:38 +0300

    fix identifyViewer attribValue

--- components/IdentifyViewer.jsx
+++ components/IdentifyViewer.jsx
@@ -629,7 +629,7 @@ class IdentifyViewer extends React.Component {
     }
     attribValue = (text, attrName, layer, result) => {
         text = "" + text; // Ensure text is a string
-        text = this.props.attributeTransform(attrName, text, layer, result);
+        text = this.props.attributeTransform(text, attrName, layer, result);
         text = MiscUtils.addLinkAnchors(text);
         return ReactHtmlParser(text, {transform: (node, index) => {
             if(node.name === "a") {
