e8176bdffb7d98595751542eab43c3290731a26e
Sandro Mani <manisandro@gmail.com>
2021-01-03 00:10:57 +0100

    Extend API with display, locate and theme actions; document extra API methods

--- plugins/API.jsx
+++ plugins/API.jsx
@@ -13,9 +13,12 @@ import {LayerRole} from '../actions/layers';
 import LayerUtils from '../utils/LayerUtils';
 import ServiceLayerUtils from '../utils/ServiceLayerUtils';
 
+import * as displayActions from '../actions/display';
 import * as layerActions from '../actions/layers';
+import * as locateActions from '../actions/locate';
 import * as mapActions from '../actions/map';
 import * as taskActions from '../actions/task';
+import * as themeActions from '../actions/theme';
 import * as windowsActions from '../actions/windows';
 
 
@@ -39,6 +42,11 @@ class API extends React.Component {
     render() {
         return null;
     }
+    /*
+     * Convenience method for adding an external layer.
+     * - resource: An external resource of the form `wms:<service_url>#<layername>` or `wmts:<capabilities_url>#<layername>`.
+     * - beforeLayerName: Insert the new layer before the layer with the specified name. If `null` or the layer does not exist, the layer is inserted on top.
+     */
     addExternalLayer = (resource, beforeLayerName = null) => {
         const params = LayerUtils.splitLayerUrlParam(resource);
         ServiceLayerUtils.findLayers(params.type, params.url, [params], this.props.mapCrs, (id, layer) => {
@@ -47,6 +55,14 @@ class API extends React.Component {
             }
         });
     }
+    /*
+     * Draw scratch geometries, and return these as GeoJSON to the calling application.
+     * - geomType: `Point`, `LineString`, `Polygon`, `Circle` or `Box`.
+     * - message: A descriptive string to display in the tool taskbar.
+     * - drawMultiple: Whether to allow drawing multiple geometries.
+     * - callback: A `function(result, crs)`, the `result` being an array of GeoJSON features, and `crs` the projection of the feature coordinates.
+     * - style: Optional, a custom style object to use for the drawn features, in the same format as `DEFAULT_FEATURE_STYLE` in `qwc2/utils/FeatureStyles.js`.
+     */
     drawScratch = (geomType, message, drawMultiple, callback, style = null) => {
         this.props.setCurrentTask("ScratchDrawing", null, null, {geomType, message, drawMultiple, callback, style});
     }
@@ -64,8 +80,11 @@ function extractFunctions(obj) {
 export default connect(state => ({
     mapCrs: state.map.projection
 }), {
+    ...extractFunctions(displayActions),
     ...extractFunctions(layerActions),
+    ...extractFunctions(locateActions),
     ...extractFunctions(mapActions),
     ...extractFunctions(taskActions),
+    ...extractFunctions(themeActions),
     ...extractFunctions(windowsActions)
 })(API);
