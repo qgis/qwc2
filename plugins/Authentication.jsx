b92312e404fad20587447c33f780fab0dc666535
Sandro Mani <manisandro@gmail.com>
2021-01-12 16:13:39 +0100

    Add support for idleTimeout option for Authentication plugin to automatically log out after a specified idle interval

--- plugins/Authentication.jsx
+++ plugins/Authentication.jsx
@@ -15,7 +15,21 @@ import ConfigUtils from '../utils/ConfigUtils';
 class Authentication extends React.Component {
     static propTypes = {
         clearLayerParam: PropTypes.bool,
+        idleTimeout: PropTypes.number,
         task: PropTypes.string
+    }
+    constructor(props) {
+        super(props);
+        this.idleTimer = null;
+    }
+    componentDidMount() {
+        const username = ConfigUtils.getConfigProp("username");
+        if (this.props.idleTimeout && username) {
+            this.idleTimer = setTimeout(this.idleAutologout, this.props.idleTimeout * 1000);
+            window.addEventListener('keydown', this.resetIdleTimer, {passive: true});
+            window.addEventListener('mousedown', this.resetIdleTimer, {passive: true});
+            window.addEventListener('wheel', this.resetIdleTimer, {passive: true});
+        }
     }
     componentDidUpdate(prevProps, prevState) {
         if (this.props.task !== prevProps.task) {
@@ -30,6 +44,21 @@ class Authentication extends React.Component {
                 window.location.href = ConfigUtils.getConfigProp("authServiceUrl") + "logout?url=" + encodeURIComponent(window.location.href);
             }
         }
+    }
+    resetIdleTimer = () => {
+        if (this.idleTimer) {
+            clearTimeout(this.idleTimer);
+            this.idleTimer = setTimeout(this.idleAutologout, this.props.idleTimeout * 1000);
+        }
+    }
+    idleAutologout = () => {
+        const urlObj = url.parse(window.location.href, true);
+        if (this.props.clearLayerParam) {
+            urlObj.query.l = undefined;
+        }
+        urlObj.search = undefined;
+        const loginUrl = ConfigUtils.getConfigProp("authServiceUrl") + "login?url=" + encodeURIComponent(url.format(urlObj));
+        window.location.href = ConfigUtils.getConfigProp("authServiceUrl") + "logout?url=" + encodeURIComponent(loginUrl);
     }
     render() {
         return null;
