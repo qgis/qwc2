2e18566012721e70b41e05d05609754e501eeda7
Sandro Mani <manisandro@gmail.com>
2021-02-01 10:59:40 +0100

    Add logic to guess country code from language code to load translations

--- actions/locale.js
+++ actions/locale.js
@@ -12,6 +12,7 @@ import localeReducer from '../reducers/locale';
 ReducerIndex.register("locale", localeReducer);
 
 import axios from 'axios';
+import {getLanguageCountries} from 'country-language';
 import ConfigUtils from '../utils/ConfigUtils';
 import {UrlParams} from '../utils/PermaLinkUtils';
 
@@ -19,7 +20,7 @@ export const CHANGE_LOCALE = 'CHANGE_LOCALE';
 
 export function loadLocale(defaultLangData) {
     return dispatch => {
-        const lang = UrlParams.getParam("lang") || (navigator ? (navigator.language || navigator.browserLanguage) : "en-US");
+        let lang = UrlParams.getParam("lang") || (navigator ? (navigator.language || navigator.browserLanguage) : "en-US");
         const config = {
             headers: {'Content-Type': 'application/json'},
             data: {}
@@ -32,11 +33,24 @@ export function loadLocale(defaultLangData) {
                 messages: response.data.messages
             });
         }).catch((e) => {
-            console.warn("Failed to load locale for " + lang + " (" + e + "), defaulting to " + defaultLangData.locale);
-            dispatch({
-                type: CHANGE_LOCALE,
-                locale: defaultLangData.locale,
-                messages: defaultLangData.messages
+            const langCode = lang.slice(0, 2).toLowerCase();
+            const countries = getLanguageCountries(langCode);
+            const country = countries.find(entry => entry.code_2 === langCode.toUpperCase()) ? langCode.toUpperCase() : ((countries[0] || {}).code_2 || "");
+            console.warn("Failed to load locale for " + lang + " (" + e + "), trying " + langCode + "-" + country);
+            lang = langCode + "-" + country;
+            axios.get(translationsPath + '/' + lang + '.json', config).then(response => {
+                dispatch({
+                    type: CHANGE_LOCALE,
+                    locale: lang,
+                    messages: response.data.messages
+                });
+            }).catch((e2) => {
+                console.warn("Failed to load locale for " + lang + " (" + e2 + "), defaulting to " + defaultLangData.locale);
+                dispatch({
+                    type: CHANGE_LOCALE,
+                    locale: defaultLangData.locale,
+                    messages: defaultLangData.messages
+                });
             });
         });
     };
