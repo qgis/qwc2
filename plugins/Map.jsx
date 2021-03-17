2d0c64c410c0af2a82c68653dfd81db051062d7d
Sandro Mani <manisandro@gmail.com>
2021-03-17 10:24:13 +0100

    Omit selection and marker layers when picking swipe layer

--- plugins/Map.jsx
+++ plugins/Map.jsx
@@ -99,7 +99,9 @@ class MapPlugin extends React.Component {
                 // Pick candidate swipe layer according to rules
                 for (let i = renderLayers.length - 1; swipeLayer === null && i >= 0; --i) {
                     const layer = renderLayers[i];
-                    if (layer.type === "wms" && layer.params.LAYERS.split(",").length > 1) {
+                    if (layer.role >= LayerRole.USERLAYER) {
+                        continue;
+                    } else if (layer.type === "wms" && layer.params.LAYERS.split(",").length > 1) {
                         const paramLayers = layer.params.LAYERS.split(",");
                         const paramOpacities = layer.params.OPACITIES.split(",");
                         const paramStyles = layer.params.STYLES.split(",");
