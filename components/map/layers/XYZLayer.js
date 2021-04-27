6b77076056f9c73df51703695d35d2e262d12939
Benoît Blanc <benoit.blanc@oslandia.com>
2021-04-27 17:39:25 +0200

    Add XYZ layer to set up its own provider for background layers

---
+++ components/map/layers/XYZLayer.js
@@ -0,0 +1,20 @@
+/**
+ * Copyright 2021 Oslandia SAS <infos+qwc2@oslandia.com>
+ * All rights reserved.
+ *
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+import ol from 'openlayers';
+
+export default {
+    create: (options) => {
+        return new ol.layer.Tile({
+            source: new ol.source.XYZ({
+                url: options.url,
+                projection: options.projection
+            })
+        });
+    }
+};
