aedf543a42d7b288cbaf6c06b0566dc9aa66aaf5
stamogis <gis@stadt.wolfsburg.de>
2021-08-18 13:04:59 +0200

    Add tooltip translation to reset rotation button

--- components/map/OlMap.jsx
+++ components/map/OlMap.jsx
@@ -15,6 +15,7 @@ import {changeMapView, clickOnMap} from '../../actions/map';
 import {changeMousePositionState} from '../../actions/mousePosition';
 import {setCurrentTask} from '../../actions/task';
 import ConfigUtils from '../../utils/ConfigUtils';
+import LocaleUtils from '../../utils/LocaleUtils';
 import MapUtils from '../../utils/MapUtils';
 
 class OlMap extends React.Component {
@@ -61,7 +62,8 @@ class OlMap extends React.Component {
         ]);
         const controls = ol.control.defaults({
             zoom: false,
-            attributionOptions: ({collapsible: false})
+            attributionOptions: ({collapsible: false}),
+            rotateOptions: ({tipLabel: LocaleUtils.tr("map.resetRotation")})
         });
         const map = new ol.Map({
             layers: [],
