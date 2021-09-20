9e2428ebef120e5344cc75dfc906a18bc6b49c80
Sandro Mani <manisandro@gmail.com>
2021-09-20 16:42:32 +0200

    Add option to show pin in share panel

--- plugins/Share.jsx
+++ plugins/Share.jsx
@@ -9,16 +9,20 @@
 import React from 'react';
 import {connect} from 'react-redux';
 import PropTypes from 'prop-types';
+import {addMarker, removeMarker} from '../actions/layers';
 import ShareSocials from '../components/share/ShareSocials';
 import ShareQRCode from '../components/share/ShareQRCode';
 import ShareLink from '../components/share/ShareLink';
 import SideBar from '../components/SideBar';
+import ToggleSwitch from '../components/widgets/ToggleSwitch';
 import LocaleUtils from '../utils/LocaleUtils';
 import {generatePermaLink} from '../utils/PermaLinkUtils';
 import './style/Share.css';
 
 class Share extends React.Component {
     static propTypes = {
+        addMarker: PropTypes.func,
+        removeMarker: PropTypes.func,
         showLink: PropTypes.bool,
         showQRCode: PropTypes.bool,
         showSocials: PropTypes.bool,
@@ -30,7 +34,19 @@ class Share extends React.Component {
         showQRCode: true
     }
     state = {
-        location: null
+        location: null,
+        pin: false
+    }
+    componentDidUpdate(prevProps, prevState) {
+        const isVisible = this.props.state.task.id === "Share";
+        const wasVisible = prevProps.state.task.id === "Share";
+        if (isVisible !== wasVisible || this.state.pin !== prevState.pin || this.props.state.map.center !== prevProps.state.map.center) {
+            if (isVisible && this.state.pin) {
+                this.props.addMarker('sharecenter', this.props.state.map.center, '', this.props.state.map.projection);
+            } else {
+                this.props.removeMarker('sharecenter');
+            }
+        }
     }
     onShow = () => {
         this.setState({location: null});
@@ -38,11 +54,19 @@ class Share extends React.Component {
     }
     renderBody = () => {
         if (this.state.location) {
-            const shareSocials = this.props.showSocials ? <ShareSocials shareTitle="QWC2" shareUrl={this.state.location}/> : null;
-            const shareLink = this.props.showLink ? <ShareLink shareUrl={this.state.location}/> : null;
-            const shareQRCode = this.props.showQRCode ? <ShareQRCode shareUrl={this.state.location}/> : null;
+            let url = this.state.location;
+            if (this.state.pin) {
+                url += url.includes("?") ? "&hc=1" : "?hc=1";
+            }
+            const shareSocials = this.props.showSocials ? <ShareSocials shareTitle="QWC2" shareUrl={url}/> : null;
+            const shareLink = this.props.showLink ? <ShareLink shareUrl={url}/> : null;
+            const shareQRCode = this.props.showQRCode ? <ShareQRCode shareUrl={url}/> : null;
             return (
                 <div>
+                    <div className="share-option-pin">
+                        <span>{LocaleUtils.tr("share.showpin")}</span>
+                        <ToggleSwitch active={this.state.pin} onChange={active => this.setState({pin: active})} />
+                    </div>
                     {shareSocials}
                     {shareLink}
                     {shareQRCode}
@@ -67,4 +91,9 @@ class Share extends React.Component {
     }
 }
 
-export default connect(state => ({state}))(Share);
+export default connect(state => ({
+    state
+}), {
+    addMarker: addMarker,
+    removeMarker: removeMarker
+})(Share);
