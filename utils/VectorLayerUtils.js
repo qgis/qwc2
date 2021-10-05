7a5541ff094aeced6118a5ff596a398b3f4779c3
Sandro Mani <manisandro@gmail.com>
2021-10-05 19:41:26 +0200

    Print highlight param fixes

--- utils/VectorLayerUtils.js
+++ utils/VectorLayerUtils.js
@@ -31,7 +31,7 @@ const VectorLayerUtils = {
         const defaultFeatureStyle = ConfigUtils.getConfigProp("defaultFeatureStyle");
         let ensureHex = null;
         if (qgisServerVersion >= 3) {
-            ensureHex = (rgb) => (!Array.isArray(rgb) ? rgb : '#' + [255 - (rgb[3] || 1) * 255, ...rgb.slice(0, 3)].map(v => v.toString(16).padStart(2, '0')).join(''));
+            ensureHex = (rgb) => (!Array.isArray(rgb) ? rgb : '#' + [255 - (rgb.length > 3 ? rgb[3] : 1) * 255, ...rgb.slice(0, 3)].map(v => v.toString(16).padStart(2, '0')).join(''));
         } else {
             ensureHex = (rgb) => (!Array.isArray(rgb) ? rgb : ('#' + (0x1000000 + (rgb[2] | (rgb[1] << 8) | (rgb[0] << 16))).toString(16).slice(1)));
         }
@@ -70,11 +70,7 @@ const VectorLayerUtils = {
                     params.geoms.push(VectorLayerUtils.geoJSONToWkt(geometry));
                     params.labelFillColors.push(defaultFeatureStyle.textFill);
                     params.labelOultineColors.push(defaultFeatureStyle.textStroke);
-                    if (qgisServerVersion >= 3) {
-                        params.labelOutlineSizes.push(scaleFactor * (feature.styleOptions || defaultFeatureStyle).strokeWidth);
-                    } else {
-                        params.labelOutlineSizes.push(scaleFactor);
-                    }
+                    params.labelOutlineSizes.push(scaleFactor);
                     params.labelSizes.push(Math.round(10 * scaleFactor));
                 }
             }
