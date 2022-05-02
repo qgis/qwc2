754da77e102fa46f580b18671079de8d6112f5ae
Sandro Mani <manisandro@gmail.com>
2022-05-02 22:34:01 +0200

    Move SnapInteraction to separate file

--- plugins/map/SnappingSupport.jsx
+++ plugins/map/SnappingSupport.jsx
@@ -19,54 +19,10 @@ import LocaleUtils from '../../utils/LocaleUtils';
 import MapUtils from '../../utils/MapUtils';
 import IdentifyUtils from '../../utils/IdentifyUtils';
 import VectorLayerUtils from '../../utils/VectorLayerUtils';
+import SnapInteraction from './SnapInteraction';
 
 import './style/SnappingSupport.css';
 
-
-class SnapInteraction extends ol.interaction.Snap {
-    constructor(options) {
-        super(options);
-        this.layer = new ol.layer.Vector({
-            source: new ol.source.Vector(),
-            style: new ol.style.Style({
-                image: new ol.style.Circle({
-                    stroke: new ol.style.Stroke({color: '#FF0000', width: 2}),
-                    radius: 10
-                })
-            }),
-            zIndex: Infinity
-        });
-        this.currentMap = null;
-    }
-    handleEvent(evt) {
-        const result = this.snapTo(evt.pixel, evt.coordinate, evt.map);
-        this.layer.getSource().clear();
-        if (result.snapped) {
-            evt.coordinate = result.vertex.slice(0, 2);
-            evt.pixel = result.vertexPixel;
-            this.layer.getSource().addFeature(new ol.Feature({
-                geometry: new ol.geom.Point(evt.coordinate)
-            }));
-        }
-        return ol.interaction.Pointer.prototype.handleEvent.call(this, evt);
-    }
-    setMap(map) {
-        if (map) {
-            map.addLayer(this.layer);
-            this.currentMap = map;
-        } else if (this.currentMap) {
-            this.currentMap.removeLayer(this.layer);
-            this.currentMap = null;
-        }
-        super.setMap(map);
-    }
-    setActive(active) {
-        if (this.layer) {
-            this.layer.setVisible(active);
-        }
-        super.setActive(active);
-    }
-}
 
 class SnappingSupport extends React.Component {
     static propTypes = {
