afc46e8c49c780179ab81524feb88c63684d9fe5
Sandro Mani <manisandro@gmail.com>
2022-12-14 16:52:33 +0100

    Remove unused import

--- plugins/Editing.jsx
+++ plugins/Editing.jsx
@@ -11,7 +11,6 @@ import PropTypes from 'prop-types';
 import {connect} from 'react-redux';
 import isEmpty from 'lodash.isempty';
 import isEqual from 'lodash.isequal';
-import axios from 'axios';
 import uuid from 'uuid';
 import {setEditContext, clearEditContext, getFeatureTemplate} from '../actions/editing';
 import {setCurrentTask, setCurrentTaskBlocked} from '../actions/task';
