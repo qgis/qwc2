5d06f5d72760e43149ff0b321e7f582a87026013
Benoît Blanc <benoit.blanc@oslandia.com>
2023-04-25 16:52:44 +0200

    Wait themes are loaded before to set startupTask

--- components/StandardApp.jsx
+++ components/StandardApp.jsx
@@ -150,12 +150,12 @@ class AppInitComponent extends React.Component {
                     }
                 }
             });
+            const task = ConfigUtils.getConfigProp("startupTask");
+            if (task) {
+                const mapClickAction = ConfigUtils.getPluginConfig(task.key).mapClickAction;
+                this.props.setCurrentTask(task.key, task.mode, mapClickAction);
+            }
         });
-        const task = ConfigUtils.getConfigProp("startupTask");
-        if (task) {
-            const mapClickAction = ConfigUtils.getPluginConfig(task.key).mapClickAction;
-            this.props.setCurrentTask(task.key, task.mode, mapClickAction);
-        }
     };
     render() {
         return null;
