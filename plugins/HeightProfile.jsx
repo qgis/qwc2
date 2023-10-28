93165f362bd045305d18faea2ded5b47695f45df
Sandro Mani <manisandro@gmail.com>
2023-10-29 01:04:05 +0200

    Port HeightProfile plugin to chart.js

--- plugins/HeightProfile.jsx
+++ plugins/HeightProfile.jsx
@@ -11,9 +11,17 @@ import axios from 'axios';
 import PropTypes from 'prop-types';
 import {connect} from 'react-redux';
 import isEmpty from 'lodash.isempty';
-import Chartist from 'chartist';
-import ChartistComponent from 'react-chartist';
-import chartistAxisTitle from 'chartist-plugin-axistitle';
+import {Line} from "react-chartjs-2";
+import {
+    Chart as ChartJS,
+    CategoryScale,
+    LinearScale,
+    PointElement,
+    LineElement,
+    Title,
+    Tooltip,
+    Filler
+} from 'chart.js';
 import FileSaver from 'file-saver';
 import {addMarker, removeMarker} from '../actions/layers';
 import {changeMeasurementState} from '../actions/measurement';
@@ -21,10 +29,19 @@ import Icon from '../components/Icon';
 import Spinner from '../components/Spinner';
 import ConfigUtils from '../utils/ConfigUtils';
 import LocaleUtils from '../utils/LocaleUtils';
-
-import './style/HeightProfile.css';
 import MeasureUtils from '../utils/MeasureUtils';
 
+import './style/HeightProfile.css';
+
+ChartJS.register(
+    CategoryScale,
+    LinearScale,
+    PointElement,
+    LineElement,
+    Title,
+    Tooltip,
+    Filler
+);
 
 /**
  * Displays a height profile along a measured line.
@@ -51,41 +68,37 @@ class HeightProfile extends React.Component {
     static defaultProps = {
         samples: 500,
         heighProfilePrecision: 0,
-        height: 100
+        height: 125
+    };
+    state = {
+        width: window.innerWidth,
+        data: {},
+        isloading: false
     };
     constructor(props) {
         super(props);
-        this.tooltip = null;
-        this.marker = null;
-        this.plot = null;
+        this.chart = null;
     }
-    state = {
-        width: window.innerWidth,
-        data: [],
-        isloading: false
-    };
     componentDidMount() {
         window.addEventListener('resize', this.handleResize);
     }
     componentWillUnmount() {
         window.removeEventListener('resize', this.handleResize);
     }
-    exportProfile(data) {
+    exportProfile = () => {
         let csv = "";
-        let idx = 0;
         csv += "index" + "\t" + "distance" + "\t" + "elevation" + "\n";
-        this.state.data.map(() => {
-            const sample = data.series[0][idx];
+        this.state.data.x.forEach((x, idx) => {
+            const sample = {x: x, y: this.state.data.y[idx]};
             const heighProfilePrecision = this.props.heighProfilePrecision;
             const distance = Math.round(sample.x * Math.pow(10, heighProfilePrecision)) / Math.pow(10, heighProfilePrecision);
             const height = Math.round(sample.y * Math.pow(10, heighProfilePrecision)) / Math.pow(10, heighProfilePrecision);
             csv += String(idx).replace('"', '""') + "\t"
-                + parseFloat(distance).toLocaleString() + "\t"
-                + parseFloat(height).toLocaleString() + "\n";
-            idx += 1;
+                + String(distance) + "\t"
+                + String(height) + "\n";
         });
         FileSaver.saveAs(new Blob([csv], {type: "text/plain;charset=utf-8"}), "heightprofile.csv");
-    }
+    };
     handleResize = () => {
         this.setState({width: window.innerWidth});
     };
@@ -94,7 +107,7 @@ class HeightProfile extends React.Component {
             if (this.props.measurement.drawing === false && this.props.measurement.geomType === "LineString" && !isEmpty(this.props.measurement.coordinates) ) {
                 this.queryElevations(this.props.measurement.coordinates, this.props.measurement.segment_lengths, this.props.projection);
             } else if (!isEmpty(this.state.data)) {
-                this.setState({data: [], pickPositionCallback: null});
+                this.setState({data: {}, pickPositionCallback: null});
             }
         }
     }
@@ -104,7 +117,12 @@ class HeightProfile extends React.Component {
             this.setState({ isloading: true });
             axios.post(serviceUrl + '/getheightprofile', {coordinates, distances, projection, samples: this.props.samples}).then(response => {
                 this.setState({ isloading: false });
-                this.setState({data: response.data.elevations});
+                const data = {
+                    x: response.data.elevations.map((entry, idx, a) => (idx / (a.length - 1) * this.props.measurement.length).toFixed(0)),
+                    y: response.data.elevations,
+                    maxY: Math.max(...response.data.elevations)
+                };
+                this.setState({data: data});
                 this.props.changeMeasurementState({...this.props.measurement, pickPositionCallback: this.pickPositionCallback});
             }).catch(e => {
                 this.setState({ isloading: false });
@@ -134,77 +152,82 @@ class HeightProfile extends React.Component {
         const distanceStr = LocaleUtils.tr("heightprofile.distance");
         const heightStr = LocaleUtils.tr("heightprofile.height");
         const aslStr = LocaleUtils.tr("heightprofile.asl");
-        const totLength = this.props.measurement.length;
-
-        // Compute tick positions (so that there are approx 10 ticks on desktop and 5 ticks on mobile on the x-axis)
-        const base = Math.pow(10, Math.floor(Math.log10(totLength / 10))); // 10E<num_digits_totLength - 1>
-        const inc = Math.round((totLength / (this.props.mobile ? 5 : 10)) / base) * base;
-        const ticks = [];
-        for (let i = 0; i < totLength; i += inc) {
-            ticks.push(i);
-        }
 
         const data = {
-            labels: new Array(this.props.samples).fill(0), // Just a dummy array of the right length, value is computed in labelInterpolationFnc
-            series: [this.state.data.map((entry, index) => ({
-                x: index * totLength / this.props.samples,
-                y: entry
-            }))]
-        };
-        const minHeight = Math.min(...this.state.data);
-        const maxHeight = Math.max(...this.state.data);
-        const options = {
-            width: this.state.width - 20 + 'px',
-            height: this.props.height,
-            chartPadding: {left: 5, bottom: 1, top: 0},
-            showArea: true,
-            axisX: {
-                type: Chartist.FixedScaleAxis,
-                ticks: ticks
-            },
-            axisY: {
-                low: minHeight - (maxHeight - minHeight) / 10
-            },
-            plugins: [
-                chartistAxisTitle({
-                    axisX: {
-                        axisTitle: distanceStr + " [m]",
-                        axisClass: 'ct-axis-title',
-                        offset: {x: 0, y: 30},
-                        textAnchor: 'middle'
-                    },
-                    axisY: {
-                        axisTitle: heightStr + " [m " + aslStr + "]",
-                        axisClass: 'ct-axis-title',
-                        offset: {x: -10, y: 10},
-                        flipTitle: true
-                    }
-                })
+            labels: this.state.data.x,
+            datasets: [
+                {
+                    label: "Elevation",
+                    data: this.state.data.y,
+                    fill: true,
+                    backgroundColor: "rgba(255,0,0,0.5)",
+                    borderColor: "rgb(255,0,0)",
+                    borderWidth: 2,
+                    pointRadius: 0
+                }
             ]
         };
-        const listeners = {
-            draw: ev => {
-                if (ev.type === "area") {
-                    ev.element._node.addEventListener("mousemove", ev2 => {
-                        const rect = ev.element._node.getBoundingClientRect();
-                        const idx = Math.min(this.props.samples - 1, Math.round((ev2.clientX - rect.left) / rect.width * this.props.samples));
-                        const x = idx / this.props.samples * totLength;
-                        this.updateMarker(x);
-                        this.updateTooltip(x, this.state.data[idx], ev2.clientX);
-                    });
-                    ev.element._node.addEventListener("mouseout", () => {
-                        this.clearMarkerAndTooltip();
-                    });
+        // Approx 10 ticks
+        const stepSizeFact = Math.pow(10, Math.ceil(Math.log10(this.props.measurement.length / 10)));
+        const stepSize = Math.round(this.props.measurement.length / (stepSizeFact)) * stepSizeFact / 10;
+        const options = {
+            responsive: true,
+            maintainAspectRatio: false,
+            plugins: {
+                legend: {
+                    display: false
+                },
+                tooltip: {
+                    intersect: false,
+                    displayColors: false,
+                    bodyFont: {weight: 'bold'},
+                    callbacks: {
+                        title: (ctx) => (distanceStr + ": " + MeasureUtils.formatMeasurement(ctx[0].parsed.x, false, 'metric', 2)),
+                        label: (ctx) => (heightStr + ": " + ctx.parsed.y.toFixed(0) + " m " + aslStr)
+                    }
                 }
+            },
+            scales: {
+                x: {
+                    type: 'linear',
+                    ticks: {
+                        stepSize: stepSize,
+                        font: {size: 10},
+                        callback: (value) => value
+                    },
+                    title: {
+                        display: true,
+                        text: distanceStr + " [m]",
+                        padding: 0
+                    },
+                    max: Math.ceil(this.props.measurement.length)
+                },
+                y: {
+                    ticks: {
+                        font: {size: 10},
+                        callback: (value) => value
+                    },
+                    title: {
+                        display: true,
+                        text: heightStr + " [m " + aslStr + "]"
+                    },
+                    max: Math.ceil(this.state.data.maxY)
+                }
+            },
+            onHover: (evt, activeEls, chart) => {
+                const chartArea = chart.chartArea;
+                const chartX = Math.min(Math.max(evt.x - chartArea.left), chartArea.width);
+                this.updateMarker(chartX / chartArea.width * this.props.measurement.length);
             }
         };
+
         const height = 'calc(' + this.props.height + 'px + 0.5em)';
         return (
             <div id="HeightProfile" style={{height: height}}>
-                <ChartistComponent data={data} listener={listeners} options={options} ref={el => {this.plot = el; }} type="Line" />
-                <span className="height-profile-tooltip" ref={el => { this.tooltip = el; }} />
-                <span className="height-profile-marker" ref={el => { this.marker = el; }} />
-                <Icon className="export-profile-button" icon="export" onClick={() => this.exportProfile(data)}
+                <div className="height-profile-chart-container">
+                    <Line data={data} options={options} ref={el => { this.chart = el; }} />
+                </div>
+                <Icon className="export-profile-button" icon="export" onClick={this.exportProfile}
                     title={LocaleUtils.tr("heightprofile.export")} />
             </div>
         );
@@ -227,28 +250,27 @@ class HeightProfile extends React.Component {
         ];
         this.props.addMarker('heightprofile', p, '', this.props.projection, 1000001); // 1000001: one higher than the zIndex in MeasurementSupport...
     };
-    updateTooltip = (x, y, plotPos) => {
-        if (!this.tooltip) {
+    showTooltip = (idx) => {
+        if (!this.chart) {
             return;
         }
-        const distanceStr = LocaleUtils.tr("heightprofile.distance");
-        const heightStr = LocaleUtils.tr("heightprofile.height");
-        const aslStr = LocaleUtils.tr("heightprofile.asl");
-        const heighProfilePrecision = this.props.heighProfilePrecision;
-        const distance = Math.round(x * Math.pow(10, heighProfilePrecision)) / Math.pow(10, heighProfilePrecision);
-        const height = Math.round(y * Math.pow(10, heighProfilePrecision)) / Math.pow(10, heighProfilePrecision);
-        this.marker.style.visibility = this.tooltip.style.visibility = 'visible';
-        this.marker.style.left = this.tooltip.style.left = plotPos + 'px';
-        this.marker.style.bottom = '30px';
-        this.marker.style.height = (this.props.height - 30) + 'px';
-        this.tooltip.style.bottom = this.props.height + 'px';
-        this.tooltip.innerHTML = "<b>" + distanceStr + ":</b> " + distance + " m<br />" +
-                                 "<b>" + heightStr + ":</b> " + height + " m " + aslStr;
+        const chartArea = this.chart.chartArea;
+        this.chart.tooltip.setActiveElements([
+            {
+                datasetIndex: 0,
+                index: idx
+            }
+        ],
+        {
+            x: (chartArea.left + chartArea.right) / 2,
+            y: (chartArea.top + chartArea.bottom) / 2,
+        });
+        this.chart.update();
     };
     clearMarkerAndTooltip = () => {
         this.props.removeMarker('heightprofile');
-        if (this.tooltip) {
-            this.marker.style.visibility = this.tooltip.style.visibility = 'hidden';
+        if (this.chart) {
+            this.chart.tooltip.setActiveElements([], {x: 0, y: 0});
         }
     };
     pickPositionCallback = (pos) => {
@@ -256,22 +278,6 @@ class HeightProfile extends React.Component {
             this.clearMarkerAndTooltip();
             return;
         }
-        // Find ct-area path
-        if (!this.plot || !this.plot.chart) {
-            return;
-        }
-        const paths = this.plot.chart.getElementsByTagName("path");
-        let path = null;
-        for (let i = 0; i < paths.length; ++i) {
-            if (paths[i].className.baseVal === "ct-area") {
-                path = paths[i];
-                break;
-            }
-        }
-        if (!path) {
-            return;
-        }
-
         // Find sample index
         const segmentLengths = this.props.measurement.segment_lengths;
         const coo = this.props.measurement.coordinates;
@@ -287,8 +293,8 @@ class HeightProfile extends React.Component {
         }
         const totLength = this.props.measurement.length;
         const k = Math.min(1, x / totLength);
-        const idx = Math.min(this.state.data.length - 1, Math.floor(k * this.props.samples));
-        this.updateTooltip(x, this.state.data[idx], path.getBoundingClientRect().left + k * path.getBoundingClientRect().width);
+        const idx = Math.min(this.state.data.y.length - 1, Math.floor(k * this.props.samples));
+        this.showTooltip(idx);
     };
     pointOnSegment = (q, p1, p2) => {
         const tol = 1E-3;
