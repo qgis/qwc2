1e32116b24633613ed0ae9c445f43c2c42505f66
Sandro Mani <manisandro@gmail.com>
2023-11-08 17:34:03 +0100

    Use export scale instead of current map scale to compute export params

--- plugins/MapExport.jsx
+++ plugins/MapExport.jsx
@@ -173,7 +173,7 @@ class MapExport extends React.Component {
         }
         const selectedFormatConfiguration = formatConfiguration.find(entry => entry.name === this.state.selectedFormatConfiguration) || {};
         const exportParams = LayerUtils.collectPrintParams(this.props.layers, this.props.theme, mapScale, this.props.map.projection, exportExternalLayers, !!selectedFormatConfiguration.baseLayer);
-        const highlightParams = VectorLayerUtils.createPrintHighlighParams(this.props.layers, this.props.map.projection, mapScale, this.state.dpi);
+        const highlightParams = VectorLayerUtils.createPrintHighlighParams(this.props.layers, this.props.map.projection, this.state.scale, this.state.dpi);
 
         return (
             <div className="mapexport-body">
