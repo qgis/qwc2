20a11479ca9c52a6eb02d540b9e4c452f4f9f6e8
Sandro Mani <manisandro@gmail.com>
2023-12-12 23:55:45 +0100

    Port to @reduxjs/toolkit

--- stores/StandardStore.js
+++ stores/StandardStore.js
@@ -6,24 +6,13 @@
  * This source code is licensed under the BSD-style license found in the
  * LICENSE file in the root directory of this source tree.
  */
-import React from 'react';
-import {applyMiddleware, combineReducers, compose, createStore} from 'redux';
-import {createDevTools} from '@redux-devtools/core';
-import {LogMonitor} from '@redux-devtools/log-monitor';
-import {DockMonitor} from '@redux-devtools/dock-monitor';
-import immutable from 'redux-immutable-state-invariant';
-import logger from 'redux-logger';
-import thunkMiddleware from  'redux-thunk';
+import {configureStore} from '@reduxjs/toolkit';
+import {combineReducers} from 'redux';
+import {logger} from 'redux-logger';
 import merge from 'deepmerge';
 import {CHANGE_BROWSER_PROPERTIES} from '../actions/browser';
 import ReducerIndex from '../reducers/index';
 
-
-const DevTools = createDevTools(
-    <DockMonitor changePositionKey="ctrl-q" toggleVisibilityKey="ctrl-h">
-        <LogMonitor theme="tomorrow" />
-    </DockMonitor>
-);
 
 export default class StandardStore {
     static store = null;
@@ -35,34 +24,34 @@ export default class StandardStore {
         }, initialState.defaultState);
         const mobileOverride = initialState.mobile;
 
-        const rootReducer = (state, action) => {
-            let newState = {
-                ...allReducers(state, action)
-            };
-            if (actionLogger) {
-                actionLogger(action, newState, state);
-            }
-            if (action && action.type === CHANGE_BROWSER_PROPERTIES && newState.browser.mobile) {
-                newState = merge(newState, mobileOverride);
-            }
-
-            return newState;
-        };
-
-        let finalCreateStore;
         const searchParams = new URLSearchParams(window.location.search);
-        if (process.env.NODE_ENV !== "production" && (searchParams.get("debug") || "").toLowerCase() === "true") {
-            const middlewares = [immutable(), thunkMiddleware, logger];
-            finalCreateStore = compose(
-                applyMiddleware.apply(null, middlewares),
-                window.devToolsExtension ? window.devToolsExtension() : DevTools.instrument()
-            )(createStore);
-        } else {
-            finalCreateStore = applyMiddleware.apply(null, [thunkMiddleware])(createStore);
-        }
-        StandardStore.store = finalCreateStore(rootReducer, defaultState);
-    }
+        const enableDevTools = process.env.NODE_ENV !== "production" && (searchParams.get("debug") || "").toLowerCase() === "true";
+
+        StandardStore.store = configureStore({
+            devTools: enableDevTools,
+            reducer: (state, action) => {
+                let newState = {
+                    ...allReducers(state, action)
+                };
+                if (actionLogger) {
+                    actionLogger(action, newState, state);
+                }
+                if (action && action.type === CHANGE_BROWSER_PROPERTIES && newState.browser.mobile) {
+                    newState = merge(newState, mobileOverride);
+                }
+                return newState;
+            },
+            preloadedState: defaultState,
+            middleware: (getDefaultMiddleware) => {
+                const middleware = getDefaultMiddleware();
+                if (enableDevTools) {
+                    middleware.push(logger);
+                }
+                return middleware;
+            }
+        });
+    };
     static get = () => {
         return StandardStore.store;
-    }
+    };
 }
