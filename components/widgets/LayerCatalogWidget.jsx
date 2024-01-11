d5a240c631aa624c24795e16e8456bc34cb59a2e
stamogis <gis@stadt.wolfsburg.de>
2024-01-11 15:28:49 +0100

    Add option for the indent size behavior in layer catalog (#292)

--- components/widgets/LayerCatalogWidget.jsx
+++ components/widgets/LayerCatalogWidget.jsx
@@ -15,6 +15,7 @@ class LayerCatalogWidget extends React.PureComponent {
     static propTypes = {
         addLayer: PropTypes.func,
         catalog: PropTypes.array,
+        levelBasedIndentSize: PropTypes.bool,
         mapCrs: PropTypes.string,
         pendingRequests: PropTypes.number
     };
@@ -39,8 +40,9 @@ class LayerCatalogWidget extends React.PureComponent {
         }
         const type = entry.resource ? entry.resource.slice(0, entry.resource.indexOf(":")) : entry.type;
         const key = (entry.resource || (entry.type + ":" + entry.name)) + ":" + idx;
+        const indentSize = !this.props.levelBasedIndentSize && level > 0 ? 1.5 : level;
         return (
-            <div key={key} style={{paddingLeft: level + 'em'}}>
+            <div key={key} style={{paddingLeft: indentSize + 'em'}}>
                 <div className="layer-catalog-widget-entry">
                     {hasSublayers ? (<Icon icon={entry.expanded ? 'tree_minus' : 'tree_plus'} onClick={() => this.toggleLayerListEntry(path)} />) : null}
                     <span onClick={() => type ? this.addServiceLayer(entry) : this.toggleLayerListEntry(path)}>
