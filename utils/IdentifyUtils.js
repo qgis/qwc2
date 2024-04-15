1ecd0adf29da79ad52a3a541ddba8d66b18985b7
Sandro Mani <manisandro@gmail.com>
2024-04-15 16:49:26 +0200

    Pass format in identify request, some services require it

--- utils/IdentifyUtils.js
+++ utils/IdentifyUtils.js
@@ -69,6 +69,7 @@ function identifyRequestParams(layer, queryLayers, projection, params) {
             styles: queryStyles,
             srs: projection,
             crs: projection,
+            format: layer.format ?? 'image/png',
             info_format: format,
             with_geometry: true,
             with_maptip: false,
