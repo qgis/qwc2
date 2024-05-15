3bc98984df71ea38b260f9aa11384490f9e9e123
Sandro Mani <manisandro@gmail.com>
2024-05-15 11:25:11 +0200

    Fix NaN currentTimeStamp if no timeData values are available

--- plugins/TimeManager.jsx
+++ plugins/TimeManager.jsx
@@ -267,11 +267,10 @@ class TimeManager extends React.Component {
             const enddate = timeData.values.length > 0 ? timeData.values[timeData.values.length - 1].hour(23).minute(59).second(59) : null;
             this.setState((state) => ({
                 timeData: timeData,
-                currentTimestamp: state.currentTimestamp ?? +timeData.values[0],
+                currentTimestamp: state.currentTimestamp ?? (timeData.values.length > 0 ? +timeData.values[0] : null),
                 startTime: timeData.values.length > 0 ? timeData.values[0].hour(0).minute(0).second(0) : null,
                 endTime: enddate && enddate.year() !== DUMMY_END_DATE.getFullYear() ? enddate : null
             }));
-            this.updateLayerTimeDimensions(timeData, this.state.currentTimestamp);
             this.updateTimeFeatures(timeData);
         } else {
             if (this.state.currentTimestamp !== prevState.currentTimestamp || this.state.timeEnabled !== prevState.timeEnabled) {
