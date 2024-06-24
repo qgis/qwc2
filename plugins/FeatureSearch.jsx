097abdecff557ffe88c935d4fdf293b24d3f1953
Sandro Mani <manisandro@gmail.com>
2024-06-24 09:51:08 +0200

    Replace all placeholder keys in FeatureSearch

--- plugins/FeatureSearch.jsx
+++ plugins/FeatureSearch.jsx
@@ -204,7 +204,7 @@ class FeatureSearch extends React.Component {
         };
         Object.keys(filter).forEach(layer => {
             Object.entries(values).forEach(([key, value]) => {
-                filter[layer] = filter[layer].replace(`$${key}$`, value.replace("'", "\\'"));
+                filter[layer] = filter[layer].replaceAll(`$${key}$`, value.replace("'", "\\'"));
             });
             params.LAYERS.push(layer);
             params.FILTER.push(layer + ":" + filter[layer]);
