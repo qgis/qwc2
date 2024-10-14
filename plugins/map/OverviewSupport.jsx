711c478f22cbc6fbcc18fcd9b2d1f5df707252b4
Sandro Mani <manisandro@gmail.com>
2024-10-14 23:19:52 +0200

    Rework OverviewSupport component

--- plugins/map/OverviewSupport.jsx
+++ plugins/map/OverviewSupport.jsx
@@ -41,33 +41,16 @@ class OverviewMap extends React.Component {
         overviewView: null,
         overviewLayer: null
     };
+    static defaultProps = {
+        options: {}
+    };
     constructor(props) {
         super(props);
-        const opt = {
-            className: "overview-map",
-            collapseLabel: '\u00BB',
-            label: '\u00AB',
-            collapsed: true,
-            collapsible: true,
-            ...props.options
-        };
-        delete opt.layer;
-        delete opt.viewOptions;
-
-        this.overview = new ol.control.OverviewMap(opt);
-        this.overview.getOverviewMap().set('id', 'overview');
+        this.overview = null;
     }
     componentDidUpdate(prevProps, prevState) {
         if (this.props.theme !== prevProps.theme) {
-            const overviewView = new ol.View({
-                projection: this.props.projection,
-                ...(this.props.options.viewOptions || {})
-            });
-            this.overview.getOverviewMap().setView(overviewView);
-            overviewView.setZoom(this.props.zoom * 0.8);
-            overviewView.setCenter(this.props.center);
-
-            this.setState({overviewView: overviewView, overviewLayer: null});
+            this.setupView();
         } else if (this.props.layers !== prevProps.layers || this.state.overviewView !== prevState.overviewView) {
             const overviewLayerName = (this.props.theme?.backgroundLayers || []).find(entry => entry.overview)?.name;
             let overviewLayer = null;
@@ -90,24 +73,50 @@ class OverviewMap extends React.Component {
             this.setState({overviewLayer: overviewLayer});
         }
     }
-    componentDidMount() {
-        this.overviewContainer = document.createElement("div");
-        this.overviewContainer.id = this.props.map.get('id') + "-overview";
-        document.getElementById("PluginsContainer").appendChild(this.overviewContainer);
-        this.overview.setTarget(this.props.map.get('id') + "-overview");
-        this.props.map.addControl(this.overview);
-        this.componentDidUpdate(this.props, {});
-    }
+    initOverviewMap = (el) => {
+        if (el) {
+            const opt = {
+                className: "overview-map",
+                collapseLabel: '\u00BB',
+                label: '\u00AB',
+                collapsed: true,
+                collapsible: true,
+                ...this.props.options
+            };
+            delete opt.layer;
+            delete opt.viewOptions;
+
+            this.overview = new ol.control.OverviewMap(opt);
+            this.overview.setTarget(el);
+            this.props.map.addControl(this.overview);
+            this.setupView();
+        }
+    };
+    setupView = () => {
+        const overviewView = new ol.View({
+            projection: this.props.projection,
+            ...(this.props.options.viewOptions || {})
+        });
+        this.overview.getOverviewMap().setView(overviewView);
+        overviewView.setZoom(this.props.zoom * 0.8);
+        overviewView.setCenter(this.props.center);
+
+        this.setState({overviewView: overviewView, overviewLayer: null});
+    };
     componentWillUnmount = () => {
-        document.getElementById("PluginsContainer").removeChild(this.overviewContainer);
+        if (this.overview) {
+            this.props.map.removeControl(this.overview);
+        }
     };
     render() {
-        if (this.state.overviewLayer) {
-            return (
-                <OlLayer key={this.state.overviewLayer.uuid} map={this.overview.getOverviewMap()} options={this.state.overviewLayer} projection={this.props.projection} />
-            );
-        }
-        return null;
+        return [
+            (
+                <div key="OverviewMap" ref={this.initOverviewMap} />
+            ),
+            this.state.overviewLayer ? (
+                <OlLayer key={this.state.overviewLayer.id} map={this.overview.getOverviewMap()} options={this.state.overviewLayer} projection={this.props.projection} />
+            ) : null
+        ];
     }
 }
 
