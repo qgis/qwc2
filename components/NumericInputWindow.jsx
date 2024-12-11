860d8a479cab5f1e6a7c35519d94ec1404df691b
Sandro Mani <manisandro@gmail.com>
2024-12-11 16:58:52 +0100

    Round entered numbers to the allowed number of decimals in NumerInputWindow

--- components/NumericInputWindow.jsx
+++ components/NumericInputWindow.jsx
@@ -96,20 +96,21 @@ class NumericInputWindow extends React.Component {
         const decimals = CoordinatesUtils.getPrecision(this.state.displayCrs);
         const value = ordinate.toFixed(decimals);
         return (
-            <TextInput onChange={(text) => this.onValueChanged(text, onChange)} value={value} />
+            <TextInput onChange={(text) => this.onValueChanged(text, decimals, onChange)} value={value} />
         );
     };
     renderMeasureInput = (measure, onChange, decimals = null) => {
         decimals = decimals ?? ConfigUtils.getConfigProp("measurementPrecision", null, 2);
         const value = measure.toFixed(decimals);
         return (
-            <TextInput onChange={(text) => this.onValueChanged(text, onChange)} value={value} />
+            <TextInput onChange={(text) => this.onValueChanged(text, decimals, onChange)} value={value} />
         );
     };
-    onValueChanged = (text, onChange) => {
+    onValueChanged = (text, decimals, onChange) => {
         const number = parseFloat(text);
         if (!isNaN(number)) {
-            onChange(number);
+            const factor = Math.pow(10, decimals);
+            onChange(Math.round(number * factor) / factor);
         }
     };
     renderPointInputForm = () => {
