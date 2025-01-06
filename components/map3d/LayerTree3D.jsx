5236971153a0c4991df8c2aa52ff4804820956e6
Sandro Mani <manisandro@gmail.com>
2025-01-06 18:31:04 +0100

    Use distinct style classes for LayerTree3D

--- components/map3d/LayerTree3D.jsx
+++ components/map3d/LayerTree3D.jsx
@@ -39,46 +39,46 @@ export default class LayerTree3D extends React.Component {
     renderBody = () => {
         const sceneContext = this.props.sceneContext;
         return (
-            <div className="layertree-item-container">
-                <div className="layertree-section">{LocaleUtils.tr("layertree3d.objects")}</div>
+            <div className="layertree3d-item-container">
+                <div className="layertree3d-section">{LocaleUtils.tr("layertree3d.objects")}</div>
                 {Object.entries(sceneContext.sceneObjects).map(([objectId, entry]) => {
                     if (!entry.layertree) {
                         return null;
                     }
                     const classes = classNames({
-                        "layertree-item": true,
-                        "layertree-item-disabled": !entry.visible
+                        "layertree3d-item": true,
+                        "layertree3d-item-disabled": !entry.visible
                     });
                     return (
                         <div className={classes} key={objectId}>
-                            <Icon className="layertree-item-checkbox"
+                            <Icon className="layertree3d-item-checkbox"
                                 icon={entry.visible ? "checked" : "unchecked"}
                                 onClick={() => sceneContext.updateSceneObject(objectId, {visible: !entry.visible})}
                             />
-                            <span className="layertree-item-title" title={objectId}>{objectId}</span>
-                            <span className="layertree-item-transparency">
-                                <input className="layertree-item-transparency-slider" max="100" min="0"
+                            <span className="layertree3d-item-title" title={objectId}>{objectId}</span>
+                            <span className="layertree3d-item-transparency">
+                                <input className="layertree3d-item-transparency-slider" max="100" min="0"
                                     onChange={(ev) => sceneContext.updateSceneObject(objectId, {opacity: parseInt(ev.target.value, 10) / 100})}
                                     step="1" type="range" value={entry.opacity * 100} />
                             </span>
                         </div>
                     );
                 })}
-                <div className="layertree-section">{LocaleUtils.tr("layertree3d.layers")}</div>
+                <div className="layertree3d-section">{LocaleUtils.tr("layertree3d.layers")}</div>
                 {sceneContext.colorLayers.map(entry => {
                     const classes = classNames({
-                        "layertree-item": true,
-                        "layertree-item-disabled": !entry.visibility
+                        "layertree3d-item": true,
+                        "layertree3d-item-disabled": !entry.visibility
                     });
                     return (
                         <div className={classes} key={entry.id}>
-                            <Icon className="layertree-item-checkbox"
+                            <Icon className="layertree3d-item-checkbox"
                                 icon={entry.visibility ? "checked" : "unchecked"}
                                 onClick={() => sceneContext.updateColorLayer(entry.id, {visibility: !entry.visibility})}
                             />
-                            <span className="layertree-item-title" title={entry.title}>{entry.title}</span>
-                            <span className="layertree-item-transparency">
-                                <input className="layertree-item-transparency-slider" max="255" min="0"
+                            <span className="layertree3d-item-title" title={entry.title}>{entry.title}</span>
+                            <span className="layertree3d-item-transparency">
+                                <input className="layertree3d-item-transparency-slider" max="255" min="0"
                                     onChange={(ev) => sceneContext.updateColorLayer(entry.id, {opacity: parseInt(ev.target.value, 10)})}
                                     step="1" type="range" value={entry.opacity} />
                             </span>
