f62d558c60bb0cfc2eb340c25a85155ce3da3426
Sandro Mani <manisandro@gmail.com>
2025-01-15 15:40:02 +0100

    Split out 3D OrbitControls into separate component

--- components/map3d/Map3D.jsx
+++ components/map3d/Map3D.jsx
@@ -10,7 +10,6 @@ import React from 'react';
 import {connect} from 'react-redux';
 
 import Instance from '@giro3d/giro3d/core/Instance.js';
-import Coordinates from '@giro3d/giro3d/core/geographic/Coordinates';
 import Extent from '@giro3d/giro3d/core/geographic/Extent.js';
 import ElevationLayer from '@giro3d/giro3d/core/layer/ElevationLayer.js';
 import Map from '@giro3d/giro3d/entities/Map.js';
@@ -19,8 +18,8 @@ import GeoTIFFSource from "@giro3d/giro3d/sources/GeoTIFFSource.js";
 import Tiles3DSource from "@giro3d/giro3d/sources/Tiles3DSource.js";
 import {fromUrl} from "geotiff";
 import PropTypes from 'prop-types';
-import {Vector2, Vector3, CubeTextureLoader, Group, Raycaster} from 'three';
-import {MapControls} from 'three/addons/controls/MapControls';
+import {Vector2, CubeTextureLoader, Group, Raycaster, Mesh} from 'three';
+import {v1 as uuidv1} from 'uuid';
 
 import {LayerRole} from '../../actions/layers';
 import {setMapCrs} from '../../actions/map3d';
@@ -28,7 +27,6 @@ import {setCurrentTask} from '../../actions/task';
 import {BackgroundSwitcher} from '../../plugins/BackgroundSwitcher';
 import ConfigUtils from '../../utils/ConfigUtils';
 import CoordinatesUtils from '../../utils/CoordinatesUtils';
-import MiscUtils from '../../utils/MiscUtils';
 import Icon from '../Icon';
 import BottomBar3D from './BottomBar3D';
 import Compare3D from './Compare3D';
@@ -37,6 +35,7 @@ import LayerTree3D from './LayerTree3D';
 import Map3DLight from './Map3DLight';
 import MapExport3D from './MapExport3D';
 import Measure3D from './Measure3D';
+import OrbitControls3D from './OrbitControls3D';
 import OverviewMap3D from './OverviewMap3D';
 import TopBar3D from './TopBar3D';
 import LayerRegistry from './layers/index';
@@ -46,11 +45,12 @@ import './style/Map3D.css';
 class UnloadWrapper extends React.Component {
     static propTypes = {
         children: PropTypes.oneOfType([PropTypes.node, PropTypes.func]),
+        key: PropTypes.string,
         onUnload: PropTypes.func
     };
     onUnload = (el) => {
         if (!el) {
-            this.props.onUnload();
+            this.props.onUnload(this.props.key);
         }
     };
     render() {
@@ -115,7 +115,8 @@ class Map3D extends React.Component {
             setViewToExtent: (bounds, angle) => {},
             getTerrainHeight: (scenePos) => {},
             getSceneIntersection: (x, y) => {}
-        }
+        },
+        sceneId: null
     };
     constructor(props) {
         super(props);
@@ -125,7 +126,6 @@ class Map3D extends React.Component {
         this.map = null;
         this.sceneObjectGroup = null;
         this.objectMap = {};
-        this.animationInterrupted = false;
         this.state.sceneContext.addLayer = this.addLayer;
         this.state.sceneContext.getLayer = this.getLayer;
         this.state.sceneContext.removeLayer = this.removeLayer;
@@ -134,7 +134,6 @@ class Map3D extends React.Component {
         this.state.sceneContext.getSceneObject = this.getSceneObject;
         this.state.sceneContext.removeSceneObject = this.removeSceneObject;
         this.state.sceneContext.updateSceneObject = this.updateSceneObject;
-        this.state.sceneContext.setViewToExtent = this.setViewToExtent;
         this.state.sceneContext.getTerrainHeight = this.getTerrainHeight;
         this.state.sceneContext.getSceneIntersection = this.getSceneIntersection;
     }
@@ -333,32 +332,12 @@ class Map3D extends React.Component {
             <div className="map3d-body">
                 <div className="map3d-map" onMouseDown={this.stopAnimations} ref={this.setupContainer} style={style} />
                 {this.state.sceneContext.scene ? (
-                    <UnloadWrapper onUnload={this.disposeInstance}>
+                    <UnloadWrapper key={this.state.sceneId} onUnload={this.onUnload}>
                         <BackgroundSwitcher bottombarHeight={10} changeLayerVisibility={this.setBaseLayer} layers={this.state.sceneContext.baseLayers} />
                         <TopBar3D options={this.props.options} sceneContext={this.state.sceneContext} searchProviders={this.props.searchProviders} />
                         <LayerTree3D sceneContext={this.state.sceneContext} />
                         <BottomBar3D sceneContext={this.state.sceneContext} />
-                        <div className="map3d-nav-widget map3d-nav-pan">
-                            <span />
-                            <Icon icon="chevron-up" onMouseDown={(ev) => this.pan(ev, 0, 1)} />
-                            <span />
-                            <Icon icon="chevron-left" onMouseDown={(ev) => this.pan(ev, 1, 0)} />
-                            <Icon icon="home" onClick={() => this.home()} />
-                            <Icon icon="chevron-right" onMouseDown={(ev) => this.pan(ev, -1, 0)} />
-                            <span />
-                            <Icon icon="chevron-down" onMouseDown={(ev) => this.pan(ev, 0, -1)} />
-                            <span />
-                        </div>
-                        <div className="map3d-nav-widget map3d-nav-rotate">
-                            <span />
-                            <Icon icon="tilt-up" onMouseDown={(ev) => this.tilt(ev, 0, 1)} />
-                            <span />
-                            <Icon icon="tilt-left" onMouseDown={(ev) => this.tilt(ev, 1, 0)} />
-                            <Icon icon="point" onClick={() => this.setViewTopDown()} />
-                            <Icon icon="tilt-right" onMouseDown={(ev) => this.tilt(ev, -1, 0)} />
-                            <span />
-                            <Icon icon="tilt-down" onMouseDown={(ev) => this.tilt(ev, 0, -1)} />
-                            <span />
+                        <OrbitControls3D ref={this.setupControls} sceneContext={this.state.sceneContext} />
                         </div>
                         <OverviewMap3D baseLayer={baseLayer} sceneContext={this.state.sceneContext} />
                         <Map3DLight sceneContext={this.state.sceneContext} />
@@ -406,18 +385,9 @@ class Map3D extends React.Component {
         });
         this.instance.add(this.map);
 
-        // Setup controls
-        const controls = new MapControls(this.instance.view.camera, this.instance.domElement);
-        controls.zoomToCursor = true;
-        controls.enableDamping = true;
-        controls.dampingFactor = 0.2;
-        controls.maxPolarAngle = Math.PI * 0.5;
-        this.instance.view.setControls(controls);
-
+        // Setup camera
         const center = extent.center();
         this.instance.view.camera.position.set(center.x, center.y, 0.5 * (extent.east - extent.west));
-        this.instance.view.controls.target = extent.centerAsVector3();
-        this.instance.view.controls.addEventListener('change', this.updateControlsTarget);
 
         // Skybox
         const cubeTextureLoader = new CubeTextureLoader();
@@ -500,11 +470,11 @@ class Map3D extends React.Component {
                 dtmCrs: demCrs,
                 baseLayers: baseLayers,
                 colorLayers: colorLayers,
-                sceneObjects: sceneObjects
-            }
+                sceneObjects: sceneObjects,
+                firstPersonControls: false
+            },
+            sceneId: uuidv1()
         }));
-
-        this.setViewToExtent(this.props.mapBBox.bounds, this.props.mapBBox.rotation);
 
         // this.inspector = Inspector.attach("map3dinspector", this.instance);
     };
@@ -513,7 +483,6 @@ class Map3D extends React.Component {
             this.inspector.detach();
         }
         this.map.dispose({disposeLayers: true});
-        this.instance.view.controls.dispose();
         this.instance.dispose();
         Object.values(this.objectMap).forEach(object => {
             this.instance.remove(object);
@@ -529,159 +498,19 @@ class Map3D extends React.Component {
         }));
         this.props.setCurrentTask(null);
     };
-    setViewToExtent = (bounds, angle = 0) => {
-        const center = {
-            x: 0.5 * (bounds[0] + bounds[2]),
-            y: 0.5 * (bounds[1] + bounds[3])
-        };
-        const elevationResult = this.map.getElevation({coordinates: new Coordinates(this.props.projection, center.x, center.y)});
-        elevationResult.samples.sort((a, b) => a.resolution > b.resolution);
-        center.z = elevationResult.samples[0]?.elevation || 0;
-
-        // Camera height to width bbox width
-        const fov = 35 / 180 * Math.PI;
-        const cameraHeight = (bounds[2] - bounds[0]) / (2 * Math.tan(fov / 2));
-
-        // Animate from old to new position/target
-        const oldPosition = this.instance.view.camera.position.clone();
-        const oldTarget = this.instance.view.controls.target.clone();
-        const oldYaw = this.instance.view.controls.getAzimuthalAngle();
-        const newPosition = new Vector3(center.x, center.y, center.z + cameraHeight);
-        const newTarget = new Vector3(center.x, center.y, center.z);
-        let rotateAngle = -oldYaw + angle;
-        while (rotateAngle > Math.PI) {
-            rotateAngle -= 2 * Math.PI;
+    onUnload = (key) => {
+        // Ensure scene has not already been disposed
+        if (this.state.sceneId === key) {
+            this.disposeInstance();
         }
-        while (rotateAngle < -Math.PI) {
-            rotateAngle += 2 * Math.PI;
-        }
-        const startTime = new Date() / 1000;
-
-        this.animationInterrupted = false;
-        const animate = () => {
-            if (!this.instance || this.animationInterrupted) {
-                return;
-            }
-            const duration = 2;
-            const elapsed = new Date() / 1000 - startTime;
-            const x = elapsed / duration;
-            const k =  0.5 * (1 - Math.cos(x * Math.PI));
-
-            const currentPosition = new Vector3().lerpVectors(oldPosition, newPosition, k);
-            const currentTarget = new Vector3().lerpVectors(oldTarget, newTarget, k);
-            currentPosition.x -= currentTarget.x;
-            currentPosition.y -= currentTarget.y;
-            currentPosition.applyAxisAngle(new Vector3(0, 0, 1), rotateAngle * k);
-            currentPosition.x += currentTarget.x;
-            currentPosition.y += currentTarget.y;
-            this.instance.view.camera.position.copy(currentPosition);
-            this.instance.view.controls.target.copy(currentTarget);
-            this.instance.view.controls.update();
-
-            if (elapsed < duration) {
-                requestAnimationFrame(animate);
-            } else {
-                this.instance.view.camera.position.copy(newPosition);
-                this.instance.view.controls.target.copy(newTarget);
-                this.instance.view.controls._rotateLeft(-angle);
-                this.instance.view.controls.update();
-            }
-        };
-        requestAnimationFrame(animate);
     };
-    home = () => {
-        const bounds = CoordinatesUtils.reprojectBbox(this.props.theme.bbox.bounds, this.props.theme.bbox.crs, this.props.projection);
-        this.setViewToExtent(bounds);
-    };
-    pan = (ev, dx, dy) => {
-        MiscUtils.killEvent(ev);
-        // Pan faster the heigher one is above the terrain
-        const d = (100 + (this.instance.view.camera.position.z - this.instance.view.controls.target.z) / 250);
-        const delta = new Vector2(dx, dy).multiplyScalar(d);
-        this.animationInterrupted = false;
-        let lastTimestamp = new Date() / 1000;
-        const animate = () => {
-            if (this.animationInterrupted) {
-                return;
-            }
-            // Pan <delta> distance per second
-            const timestamp = new Date() / 1000;
-            const k = timestamp - lastTimestamp;
-            lastTimestamp = timestamp;
-            this.instance.view.controls._pan(delta.x * k, delta.y * k);
-            this.instance.view.controls.update();
-            requestAnimationFrame(animate);
-        };
-        requestAnimationFrame(animate);
-        ev.view.addEventListener("mouseup", () => {
-            this.animationInterrupted = true;
-        }, {once: true});
-    };
-    setViewTopDown = () => {
-        // Animate from old to new position
-        const target = this.instance.view.controls.target;
-        const oldPosition = this.instance.view.camera.position.clone();
-        const oldYaw = this.instance.view.controls.getAzimuthalAngle();
-        const newPosition = new Vector3(target.x, target.y, target.distanceTo(oldPosition));
-        const startTime = new Date() / 1000;
+    setupControls = (instance) => {
+        this.setState(state => ({sceneContext: {
+            ...state.sceneContext,
+            setViewToExtent: instance?.setViewToExtent
 
-        this.animationInterrupted = false;
-        const animate = () => {
-            if (!this.instance || this.animationInterrupted) {
-                return;
-            }
-            const duration = 2;
-            const elapsed = new Date() / 1000 - startTime;
-            const x = elapsed / duration;
-            const k =  0.5 * (1 - Math.cos(x * Math.PI));
-
-            const currentPosition = new Vector3().lerpVectors(oldPosition, newPosition, k);
-            currentPosition.x -= target.x;
-            currentPosition.y -= target.y;
-            currentPosition.applyAxisAngle(new Vector3(0, 0, 1), -oldYaw * k);
-            currentPosition.x += target.x;
-            currentPosition.y += target.y;
-            this.instance.view.camera.position.copy(currentPosition);
-            this.instance.view.controls.update();
-
-            if (elapsed < duration) {
-                requestAnimationFrame(animate);
-            } else {
-                this.instance.view.camera.position.copy(newPosition);
-                this.instance.view.controls.update();
-            }
-        };
-        requestAnimationFrame(animate);
-    };
-    tilt = (ev, yaw, az) => {
-        MiscUtils.killEvent(ev);
-        // Pan faster the heigher one is above the terrain
-        this.animationInterrupted = false;
-        let lastTimestamp = new Date() / 1000;
-        const animate = () => {
-            if (this.animationInterrupted) {
-                return;
-            }
-            // Pan <delta> distance per second
-            const timestamp = new Date() / 1000;
-            const k = timestamp - lastTimestamp;
-            lastTimestamp = timestamp;
-            if (az) {
-                this.instance.view.controls._rotateUp(az * k);
-            }
-            if (yaw) {
-                this.instance.view.controls._rotateLeft(yaw * k);
-            }
-            this.instance.view.controls.update();
-            requestAnimationFrame(animate);
-        };
-        requestAnimationFrame(animate);
-        ev.view.addEventListener("mouseup", () => {
-            this.animationInterrupted = true;
-        }, {once: true});
-    };
-    stopAnimations = () => {
-        this.animationInterrupted = true;
+        }}));
+        instance?.setViewToExtent?.(this.props.mapBBox.bounds, this.props.mapBBox.rotation);
     };
     getTerrainHeight = (scenePos) => {
         const dtmPos = CoordinatesUtils.reproject(scenePos, this.state.sceneContext.mapCrs, this.state.sceneContext.dtmCrs);
@@ -723,18 +552,6 @@ class Map3D extends React.Component {
         }
         return objInter ?? terrInter;
     };
-    updateControlsTarget = () => {
-        const x = this.instance.view.camera.position.x;
-        const y = this.instance.view.camera.position.y;
-        const elevationResult = this.map.getElevation({coordinates: new Coordinates(this.props.projection, x, y)});
-        elevationResult.samples.sort((a, b) => a.resolution > b.resolution);
-        const terrainHeight = elevationResult.samples[0]?.elevation || 0;
-        const cameraHeight = this.instance.view.camera.position.z;
-        // If camera height is at terrain height, target height should be at terrain height
-        // If camera height is at twice the terrain height or further, target height should be zero
-        const targetHeight = terrainHeight > 0 ? terrainHeight * Math.max(0, 1 - (cameraHeight - terrainHeight) / terrainHeight) : 0;
-        this.instance.view.controls.target.z = targetHeight;
-    };
     redrawScene = (ev) => {
         const width = ev.target.innerWidth;
         const height = ev.target.innerHeight;
@@ -742,6 +559,9 @@ class Map3D extends React.Component {
         this.instance.view.camera.aspect = width / height;
         this.instance.view.camera.updateProjectionMatrix();
         this.instance.renderer.render(this.instance.scene, this.instance.view.camera);
+    };
+    setViewToExtent = (bounds, rotation) => {
+        this.state.sceneContext.setViewToExtent(bounds, rotation);
     };
 }
 
