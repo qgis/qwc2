cb3472c85daf2c1b4a4cc5df12d28c86efd92fb3
Jordi <102949291+jblanchg@users.noreply.github.com>
2025-02-05 11:21:20 +0100

    Allow returning bbox/center in getResultGeometry response

--- components/SearchBox.jsx
+++ components/SearchBox.jsx
@@ -749,7 +749,7 @@ class SearchBox extends React.Component {
             // If first feature is not a point(=marker), add a marker
             if (features[0].styleName !== "marker" && !response.hidemarker) {
                 features.unshift({
-                    geometry: {type: 'Point', coordinates: CoordinatesUtils.reproject([item.x, item.y], item.crs ?? this.props.map.projection, this.props.map.projection)},
+                    geometry: {type: 'Point', coordinates: CoordinatesUtils.reproject(response.center ?? [item.x, item.y], item.crs ?? this.props.map.projection, this.props.map.projection)},
                     styleName: 'marker'
                 });
             }
@@ -762,7 +762,7 @@ class SearchBox extends React.Component {
             features[0].id = 'searchmarker';
             this.props.addLayerFeatures(layer, features, true);
         }
-        let bbox = item.bbox ?? [item.x, item.y, item.x, item.y];
+        let bbox = response.bbox ?? item.bbox ?? [item.x, item.y, item.x, item.y];
         bbox = CoordinatesUtils.reprojectBbox(bbox, item.crs ?? this.props.map.projection, this.props.map.projection);
         this.zoomToResultBBox(bbox, scale);
     };
