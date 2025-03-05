5cd7ba8dc20bb8346252c4b2aacf00a6bf0dad2b
Sandro Mani <manisandro@gmail.com>
2025-03-05 16:31:51 +0100

    Add stock viewer files

---
+++ app.jsx
@@ -0,0 +1,17 @@
+/**
+ * Copyright 2016-2021 Sourcepole AG
+ * All rights reserved.
+ *
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+import React from 'react';
+import {createRoot} from 'react-dom/client';
+import StandardApp from './components/StandardApp';
+import appConfig from './appConfig';
+import './icons/build/qwc2-icons.css';
+
+const container = document.getElementById('container');
+const root = createRoot(container);
+root.render(<StandardApp appConfig={appConfig}/>);
