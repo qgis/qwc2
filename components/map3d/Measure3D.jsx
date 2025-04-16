03be784c276715ed729dd115323bade3fac2d612
Sandro Mani <manisandro@gmail.com>
2025-04-16 19:14:29 +0200

    Add 3D height difference measure tool

--- components/map3d/Measure3D.jsx
+++ components/map3d/Measure3D.jsx
@@ -91,6 +91,7 @@ export default class Measure3D extends React.Component {
     renderModeSwitcher = () => {
         const buttons = [
             {key: "Point", label: LocaleUtils.tr("measureComponent.pointLabel")},
+            {key: "HeightDiff", label: LocaleUtils.tr("measureComponent.heightDiffLabel")},
             {key: "LineString", label: LocaleUtils.tr("measureComponent.lengthLabel")},
             {key: "Polygon", label: LocaleUtils.tr("measureComponent.areaLabel")}
         ];
@@ -121,6 +122,18 @@ export default class Measure3D extends React.Component {
                     <span className="measure-unit-label">{LocaleUtils.tr("measureComponent.absolute")}</span>
                 );
             }
+        } else if (this.state.mode === "HeightDiff") {
+            text = (this.state.result || []).length === 2 ? MeasureUtils.formatMeasurement(Math.abs(this.state.result[1].z - this.state.result[0].z), false, this.state.lenUnit) : "";
+            unitSelector = (
+                <select onChange={ev => this.setState({lenUnit: ev.target.value})} value={this.state.lenUnit}>
+                    <option value="metric">{LocaleUtils.tr("measureComponent.metric")}</option>
+                    <option value="imperial">{LocaleUtils.tr("measureComponent.imperial")}</option>
+                    <option value="m">m</option>
+                    <option value="km">km</option>
+                    <option value="ft">ft</option>
+                    <option value="mi">mi</option>
+                </select>
+            );
         } else if (this.state.mode === "LineString") {
             text = MeasureUtils.formatMeasurement(this.state.result.length, false, this.state.lenUnit);
             unitSelector = (
@@ -209,24 +222,22 @@ export default class Measure3D extends React.Component {
             this.abortController.abort();
         }
         this.abortController = new AbortController();
-        const pick = this.state.mode === 'Point' ? (
-            null // default pick
-        ) : (
-            (e) => this.props.sceneContext.scene.pickObjectsAt(e, {sortByDistance: true, where: [this.props.sceneContext.getMap()]})
-        );
+        const terrainPick = (e) => this.props.sceneContext.scene.pickObjectsAt(e, {sortByDistance: true, where: [this.props.sceneContext.getMap()]});
         const options = {
             signal: this.abortController.signal,
             endCondition: conditions.doubleClick,
-            pick: pick
+            pick: null // default pick
         };
         if (this.state.mode === 'Point') {
             this.measureTool.createPoint(options).then(this.measurePoint).catch(() => {});
         } else if (this.state.mode === 'LineString') {
+            options.pick = terrainPick;
             this.measureTool.createLineString(options).then(this.measureLine).catch(() => {});
         } else if (this.state.mode === 'Polygon') {
+            options.pick = terrainPick;
             this.measureTool.createPolygon(options).then(this.measureArea).catch(() => {});
-        } else if (this.state.mode === 'Height') {
-            this.measureTool.createVerticalMeasure(options).then(this.measureHeight).catch(() => {});
+        } else if (this.state.mode === 'HeightDiff') {
+            this.measureTool.createPoint(options).then(this.measureHeightDiff).catch(() => {});
         }
     };
     measurePoint = (point) => {
@@ -243,9 +254,9 @@ export default class Measure3D extends React.Component {
 
             const elevationLabelFormatter = (options) => {
                 if (options.index === 0) {
-                    return MeasureUtils.formatMeasurement(elevation, false, LocaleUtils.tr("measureComponent.absolute"));
+                    return MeasureUtils.formatMeasurement(elevation, false, "m") + " " + LocaleUtils.tr("measureComponent.absolute");
                 } else if (ground > 0 && this.state.elevUnit === "ground") {
-                    return MeasureUtils.formatMeasurement(pos.z - elevation, false, LocaleUtils.tr("measureComponent.ground"));
+                    return MeasureUtils.formatMeasurement(pos.z - elevation, false, "m") + " " + LocaleUtils.tr("measureComponent.ground");
                 } else {
                     return MeasureUtils.formatMeasurement(pos.z, false, LocaleUtils.tr("measureComponent.absolute"));
                 }
@@ -279,6 +290,50 @@ export default class Measure3D extends React.Component {
             // Setup for next measurement
             this.restart();
         });
+    };
+    measureHeightDiff = (point) => {
+        if (point === null) {
+            this.restart();
+            return;
+        }
+        if ((this.state.result || []).length >= 2) {
+            this.clearResult();
+        }
+        const pos = point.points[0];
+
+        if ((this.state.result || []).length === 1) {
+            // Add line if two points drawn
+            const points = [this.state.result[0], pos];
+            if (points[0].z > points[1].z) {
+                points.reverse();
+            }
+            const line = new Shape({
+                showVertexLabels: true,
+                vertexLabelFormatter: (options) => options.index === 2 ? MeasureUtils.formatMeasurement(points[1].z - points[0].z, false, this.state.lenUnit) : null,
+                showLine: true
+            });
+            line.setPoints([
+                new Vector3(points[0].x, points[0].y, points[0].z),
+                new Vector3(points[1].x, points[1].y, points[0].z),
+                new Vector3(points[1].x, points[1].y, points[1].z)
+            ]);
+            this.props.sceneContext.scene.add(line);
+            this.measurementObjects.push(line);
+        } else {
+            // Add first drawn point
+            const shape = new Shape({
+                showVertices: true
+            });
+            shape.setPoints([new Vector3(pos.x, pos.y, pos.z)]);
+            this.props.sceneContext.scene.add(shape);
+            this.measurementObjects.push(shape);
+        }
+        this.props.sceneContext.scene.remove(point);
+        this.setState(state => ({
+            result: [...(state.result || []), {x: pos.x, y: pos.y, z: pos.z}]
+        }));
+
+        this.restart();
     };
     measureLine = (lineString) => {
         if (lineString === null) {
