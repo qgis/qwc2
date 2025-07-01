8892b97f7fa7baa5268d2e76b1aec235f1e58aee
Sandro Mani <manisandro@gmail.com>
2025-07-01 11:06:08 +0200

    NumericInput3D: allow entering size also for rotated objects

--- components/map3d/drawtool/NumericInput3D.jsx
+++ components/map3d/drawtool/NumericInput3D.jsx
@@ -12,6 +12,7 @@ import ReactDOM from 'react-dom';
 import PropTypes from 'prop-types';
 import {Box3} from 'three';
 
+import CoordinatesUtils from '../../../utils/CoordinatesUtils';
 import LocaleUtils from '../../../utils/LocaleUtils';
 import ResizeableWindow from '../../ResizeableWindow';
 import NumberInput from '../../widgets/NumberInput';
@@ -56,6 +57,7 @@ export default class NumericInput3D extends React.Component {
         const scale = this.state.scale;
         const size = this.state.size;
         const disabled = !this.props.selectedObject;
+        const unit = CoordinatesUtils.getUnits(this.props.sceneContext.mapCrs);
         const contents = (
             <ResizeableWindow fitHeight icon="numericinput" initialWidth={300} initialX={-1}
                 onClose={this.props.toggleNumericInput} scrollable title={LocaleUtils.tr("draw3d.numericinput")} >
@@ -82,7 +84,7 @@ export default class NumericInput3D extends React.Component {
                             </tr>
                             {size ? (
                                 <tr>
-                                    <td>{LocaleUtils.tr("draw3d.thesize")}</td>
+                                    <td>{LocaleUtils.tr("draw3d.thesize")} [{unit}]</td>
                                     <td><NumberInput decimals={1} disabled={disabled} onChange={x => this.updateSize(0, x)} value={size[0]} /></td>
                                     <td><NumberInput decimals={1} disabled={disabled} onChange={y => this.updateSize(1, y)} value={size[1]} /></td>
                                     <td><NumberInput decimals={1} disabled={disabled} onChange={z => this.updateSize(2, z)} value={size[2]} /></td>
@@ -101,17 +103,17 @@ export default class NumericInput3D extends React.Component {
     }
     updateStateFromObject = () => {
         if (this.props.selectedObject) {
-            const isRotated =
-                this.props.selectedObject.rotation.x !== 0 ||
-                this.props.selectedObject.rotation.y !== 0 ||
-                this.props.selectedObject.rotation.z !== 0;
-            let size = null;
-            if (!isRotated) {
-                const bbox = new Box3().setFromObject(this.props.selectedObject);
-                size = [
-                    bbox.max.x - bbox.min.x, bbox.max.y - bbox.min.y, bbox.max.z - bbox.min.z
-                ];
-            }
+            // Temporarily remove rotation and compute bbox
+            const originalRotation = this.props.selectedObject.rotation.clone();
+            this.props.selectedObject.rotation.set(0, 0, 0);
+            this.props.selectedObject.updateMatrixWorld(true);
+            const bbox = new Box3().setFromObject(this.props.selectedObject);
+            const size = [
+                bbox.max.x - bbox.min.x, bbox.max.y - bbox.min.y, bbox.max.z - bbox.min.z
+            ];
+            this.props.selectedObject.rotation.copy(originalRotation);
+            this.props.selectedObject.updateMatrixWorld(true);
+
             this.setState({
                 pos: this.props.selectedObject.position.toArray(),
                 rot: this.props.selectedObject.rotation.toArray().slice(0, 3).map(x => x / Math.PI * 180),
