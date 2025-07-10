086ac5f05e77f3533061a3aac0e6c2f536e446d3
Sandro Mani <manisandro@gmail.com>
2025-07-10 23:06:03 +0200

    MiscUtils: add hslToRgb

--- utils/MiscUtils.js
+++ utils/MiscUtils.js
@@ -86,6 +86,33 @@ const MiscUtils = {
         const toHex = (num) => ("0" + Math.round(num).toString(16)).slice(-2);
         return '#' + toHex(color3[0]) + toHex(color3[1]) + toHex(color3[2]);
     },
+    hslToRgb(h, s, l) {
+        // Takes h, s, l in [0, 1] range and returns [r, g, b] in [0, 1] range
+        const c = (1 - Math.abs(2 * l - 1)) * s;
+        const x = c * (1 - Math.abs((h / 60) % 2 - 1));
+        const m = l - c / 2;
+        h *= 360;
+
+        let r = 0;
+        let g = 0;
+        let b = 0;
+
+        if (h >= 0 && h < 60) {
+            r = c; g = x; b = 0;
+        } else if (h >= 60 && h < 120) {
+            r = x; g = c; b = 0;
+        } else if (h >= 120 && h < 180) {
+            r = 0; g = c; b = x;
+        } else if (h >= 180 && h < 240) {
+            r = 0; g = x; b = c;
+        } else if (h >= 240 && h < 300) {
+            r = x; g = 0; b = c;
+        } else if (h >= 300 && h < 360) {
+            r = c; g = 0; b = x;
+        }
+
+        return [r + m, g + m, b + m];
+    },
     ensureArray(el) {
         if (el === undefined) {
             return [];
