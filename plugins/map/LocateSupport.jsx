b6c5170927b65970275b376059745fbb2a7756d2
Sandro Mani <manisandro@gmail.com>
2025-08-20 18:29:56 +0200

    LocateSupport: rewrite and simplify code

--- plugins/map/LocateSupport.jsx
+++ plugins/map/LocateSupport.jsx
@@ -11,10 +11,14 @@
 import React from 'react';
 import {connect} from 'react-redux';
 
+import ol from 'openlayers';
 import PropTypes from 'prop-types';
 
 import {changeLocateState, changeLocatePosition, onLocateError} from '../../actions/locate';
-import OlLocate from '../../components/map/OlLocate';
+import CoordinatesUtils from '../../utils/CoordinatesUtils';
+import LocaleUtils from '../../utils/LocaleUtils';
+
+import './style/LocateSupport.css';
 
 /**
  * GPS locate support for the map component.
@@ -23,73 +27,96 @@ class LocateSupport extends React.Component {
     static propTypes = {
         changeLocatePosition: PropTypes.func,
         changeLocateState: PropTypes.func,
+        /** Whether to draw an accuracy circle around the location point. */
+        drawCircle: PropTypes.bool,
         locateState: PropTypes.object,
         map: PropTypes.object,
+        /** Whether to display the accuracy in meters (`true`) or in feet (`false`). */
+        metric: PropTypes.bool,
         onLocateError: PropTypes.func,
-        options: PropTypes.object,
         projection: PropTypes.string,
-        startupParams: PropTypes.object
+        /** Whether to show a popup displaying accuracy information when clicking on the location point. */
+        showPopup: PropTypes.bool,
+        /** The geolocation startup mode. Either `DISABLED`, `ENABLED` or `FOLLOWING`. */
+        startupMode: PropTypes.string,
+        startupParams: PropTypes.object,
+        /** Whether to stop following when the map is dragged. */
+        stopFollowingOnDrag: PropTypes.bool,
+        /** Tracking options, as documented in the [HTML5 Geolocation spec](https://www.w3.org/TR/geolocation-API/#position_options_interface) */
+        trackingOptions: PropTypes.object
     };
     static defaultProps = {
-        options: {}
-    };
-    static defaultOpt = {
-        startupMode: "DISABLED", // either "DISABLED", "ENABLED" or "FOLLOWING"
-        follow: false, // follow with zoom and pan the user's location
-        remainActive: true,
+        drawCircle: true,
         metric: true,
+        showPopup: false,
+        startupMode: "DISABLED",
         stopFollowingOnDrag: false,
-        keepCurrentZoomLevel: true,
-        locateOptions: {
+        trackingOptions: {
             maximumAge: 2000,
             enableHighAccuracy: true,
-            timeout: 10000,
-            maxZoom: 18
+            timeout: 10000
         }
     };
-    componentDidMount() {
-        const options = {...LocateSupport.defaultOpt, ...this.props.options};
-        this.locate = new OlLocate(this.props.map, options);
-        this.locate.options.onLocationError = this.onLocationError;
-        this.locate.on("propertychange", (e) => {this.onPropChange(e.key, e.target.get(e.key)); });
-        this.configureLocate(this.props.locateState.state);
+    constructor(props) {
+        super(props);
+        const trackingOptions = {...LocateSupport.defaultProps.trackingOptions, ...props.trackingOptions};
 
-        const startupMode = options.startupMode.toUpperCase();
+        this.geolocate = new ol.Geolocation({
+            projection: this.props.projection,
+            trackingOptions: trackingOptions
+        });
+
+        this.posLayer = new ol.layer.Vector({
+            source: new ol.source.Vector(),
+            zIndex: 1000000,
+            style: this.locationPositionFeatureStyle
+        });
+        this.posFeature = new ol.Feature();
+        this.posFeature.set('__suppress_map_click', true);
+        this.posLayer.getSource().addFeature(this.posFeature);
+
+        this.posPopup = this.createPopup();
+        this.posOverlay = new ol.Overlay({
+            element: this.posPopup,
+            positioning: 'top-center',
+            stopEvent: true
+        });
+        this.posOverlayContents = this.posPopup.getElementsByClassName("locate-popup-contents")[0];
+        this.posPopup.getElementsByClassName("locate-popup-close")[0].onclick = () => { this.posPopup.hidden = true; };
+
+        this.requestedMode = 'DISABLED';
+    }
+    componentDidMount() {
+        const startupMode = this.props.startupMode.toUpperCase();
         const startupParams = this.props.startupParams;
         const highlightCenter = ["true", "1"].includes((startupParams.hc || "").toLowerCase());
         const searchParams = startupParams.hp || startupParams.hf || startupParams.st;
         if (startupMode !== "DISABLED" && !searchParams && !highlightCenter) {
-            this.props.changeLocateState(startupMode);
+            this.requestedMode = startupMode;
+            this.start();
         }
     }
     componentDidUpdate(prevProps) {
-        if (this.props.locateState.state !== prevProps.locateState.state) {
-            this.configureLocate(this.props.locateState.state);
+        const newState = this.props.locateState.state;
+        const oldState = prevProps.locateState.state;
+        if (newState !== oldState) {
+            if (newState === "ENABLED" || newState === "FOLLOWING") {
+                this.requestedMode = newState;
+                if (oldState === "DISABLED") {
+                    this.start();
+                }
+            } else if (newState === "DISABLED") {
+                this.requestedMode = "DISABLED";
+                this.stop();
+            }
         }
         if (this.props.projection !== prevProps.projection) {
-            this.locate.setProjection(this.props.projection);
+            this.geolocate.setProjection(this.props.projection);
         }
     }
-    configureLocate = (newState) => {
-        const state = this.locate.get("state");
-        if (newState === "ENABLED" && state === "DISABLED") {
-            this.locate.start();
-        } else if (newState === "FOLLOWING" && state === "ENABLED") {
-            this.locate.startFollow();
-        } else if (newState === "FOLLOWING" && state === "DISABLED") {
-            this.locate.start();
-            this.locate.startFollow();
-        } else if (newState === "DISABLED") {
-            this.locate.stop();
-        }
-    };
-    onPropChange = (key, value) => {
-        if (key === "state" && this.props.locateState.state !== value) {
-            this.props.changeLocateState(value);
-        } else if (key === "position" && this.props.locateState.position !== value) {
-            this.props.changeLocatePosition(value);
-        }
-    };
+    render() {
+        return null;
+    }
     onLocationError = (err) => {
         this.props.onLocateError(err.message);
         // User denied geolocation prompt
@@ -98,10 +125,107 @@ class LocateSupport extends React.Component {
         } else {
             this.props.changeLocateState("DISABLED");
         }
+        this.stop();
     };
-    render() {
-        return null;
-    }
+    start = () => {
+        this.props.changeLocateState("LOCATING");
+        this.geolocate.on('change:position', this.positionChanged);
+        this.geolocate.on('error', this.onLocationError);
+        this.geolocate.setTracking(true);
+        this.props.map.addLayer(this.posLayer);
+        this.props.map.addOverlay(this.posOverlay);
+        this.props.map.on('pointerdrag', this.maybeStopFollow);
+        this.props.map.on('click', this.maybeShowPopup);
+        this.props.map.on('touch', this.maybeShowPopup);
+        this.posPopup.hidden = true;
+        this.posLayer.setVisible(false);
+    };
+    stop = () => {
+        this.geolocate.un('change:position', this.positionChanged);
+        this.geolocate.un('error', this.onLocationError);
+        this.geolocate.setTracking(false);
+        this.props.map.removeLayer(this.posLayer);
+        this.props.map.removeOverlay(this.posOverlay);
+        this.props.map.un('pointerdrag', this.maybeStopFollow);
+        this.props.map.un('click', this.maybeShowPopup);
+        this.props.map.un('touch', this.maybeShowPopup);
+    };
+    positionChanged = () => {
+        if (this.props.locateState.state === "LOCATING") {
+            this.props.changeLocateState(this.requestedMode);
+            this.posLayer.setVisible(true);
+        }
+
+        const mapPos = this.geolocate.getPosition();
+        const wgsPos = CoordinatesUtils.reproject(mapPos, this.props.projection, "EPSG:4326");
+        this.props.changeLocatePosition(wgsPos);
+
+        const point = new ol.geom.Point(mapPos);
+        if (this.props.drawCircle) {
+            const circle = new ol.geom.Circle(mapPos, this.geolocate.getAccuracy());
+            this.posFeature.setGeometry(new ol.geom.GeometryCollection([point, circle]));
+        } else {
+            this.posFeature.setGeometry(point);
+        }
+        if (!this.posPopup.hidden) {
+            this.updatePopupContents();
+        }
+        if (this.props.locateState.state === "FOLLOWING") {
+            this.props.map.getView().setCenter(mapPos);
+        }
+    };
+    maybeStopFollow = () => {
+        if (this.props.locateState.state === "FOLLOWING" && this.props.stopFollowingOnDrag) {
+            this.props.changeLocateState("ENABLED");
+        }
+    };
+    maybeShowPopup = (ev) => {
+        if (this.props.showPopup) {
+            const feature = this.props.map.getFeaturesAtPixel(ev.pixel, {
+                layerFilter: layer => layer === this.posLayer
+            })[0];
+            if (feature) {
+                this.posPopup.hidden = false;
+                this.updatePopupContents();
+                ev.stopPropagation();
+            } else {
+                this.posPopup.hidden = true;
+            }
+        }
+    };
+    updatePopupContents = () => {
+        const accuracy = this.geolocate.getAccuracy();
+        let contents = LocaleUtils.tr("locate.popup");
+        if (this.props.metric) {
+            contents = contents.replace("{distance}", accuracy);
+            contents = contents.replace("{unit}", LocaleUtils.tr("locate.metersUnit"));
+        } else {
+            contents = contents.replace("{distance}", Math.round(accuracy * 3.2808399));
+            contents = contents.replace("{unit}", LocaleUtils.tr("locate.feetUnit"));
+        }
+        this.posOverlayContents.innerHTML = contents;
+        this.posOverlay.setPosition(this.posFeature.getGeometry().getGeometries()[0].getCoordinates());
+    };
+    locationPositionFeatureStyle = () => {
+        return new ol.style.Style({
+            image: new ol.style.Circle({
+                radius: 6,
+                fill: new ol.style.Fill({color: 'rgba(42, 147, 238, 0.7)'}),
+                stroke: new ol.style.Stroke({color: 'rgba(19, 106, 236, 1)', width: 2})
+            }),
+            fill: new ol.style.Fill({color: 'rgba(19, 106, 236, 0.15)'}),
+            stroke: new ol.style.Stroke({color: 'rgba(19, 106, 236, 1)', width: 2})
+        });
+    };
+    createPopup = ()  => {
+        const popup = document.createElement('div');
+        popup.className = "locate-popup";
+        popup.innerHTML = `
+          <a class="locate-popup-close">✖</a>
+          <div class="locate-popup-contents">
+        `;
+        return popup;
+    };
 }
 
 export default connect((state) => ({
