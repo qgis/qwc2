7a053327d09ac869c4f95ce823a60ed6c1d6f617
Sandro Mani <manisandro@gmail.com>
2025-10-08 23:20:46 +0200

    SearchProviders: add methods to dynamically register search providers

--- utils/SearchProviders.js
+++ utils/SearchProviders.js
@@ -460,7 +460,6 @@ export class FulltextSearch {
     }
 }
 
-
 /** ************************************************************************ **/
 
 const SearchProviders = {
@@ -489,9 +488,19 @@ const SearchProviders = {
     }
 };
 
-export default SearchProviders;
-
 /** ************************************************************************ **/
+
+const addedSearchProviders = new Set();
+
+export function registerSearchProvider(key, config) {
+    SearchProviders[key] = config;
+    addedSearchProviders.add(key);
+}
+
+export function unregisterSearchProvider(key) {
+    delete SearchProviders[key];
+    addedSearchProviders.delete(key);
+}
 
 // Uniformize the response of getResultGeometry
 function getResultGeometry(provider, item, callback) {
@@ -546,7 +555,8 @@ export function collectSearchProviders(theme, layers, mapScale = null) {
     const availableProviders = {};
     const themeLayerNames = layers.map(layer => layer.role === LayerRole.THEME ? layer.params.LAYERS : "").join(",").split(",").filter(entry => entry);
     const providerKeys = new Set();
-    for (let entry of theme?.searchProviders || []) {
+    const enabledProviders = [...(theme?.searchProviders || []), ...addedSearchProviders];
+    for (let entry of enabledProviders) {
         if (typeof entry === 'string') {
             entry = {provider: entry};
         }
