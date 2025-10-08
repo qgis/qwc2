8ed9ccf2c4a774cf2df3b7e846496ef8f3983bad
Sandro Mani <manisandro@gmail.com>
2025-10-08 11:15:04 +0200

    Map: keep OlLayer keys stable if possible

--- plugins/Map.jsx
+++ plugins/Map.jsx
@@ -204,13 +204,18 @@ class Map extends React.Component {
     }
     renderLayers = () => {
         let zIndex = 0;
+        const usedKeys = new Set();
         return this.state.renderLayers.map(layer => {
             if (layer.type === "placeholder") {
                 return null;
             }
             ++zIndex;
             const swipe = this.props.swipe !== null && layer === this.state.swipeLayer;
-            const key = layer.id + ":" + layer.params?.LAYERS ?? "";
+            let key = layer.id;
+            for (let i = 0; usedKeys.has(key); ++i) {
+                key = layer.id + ":" + i;
+            }
+            usedKeys.add(key);
             return (
                 <OlLayer key={key} options={layer} swipe={swipe ? this.props.swipe : null} zIndex={layer.zIndex ?? zIndex} />
             );
