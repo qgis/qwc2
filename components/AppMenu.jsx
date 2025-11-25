96876d95be5a621ac40ea7607ee43585acc38443
Sandro Mani <manisandro@gmail.com>
2025-11-25 18:03:05 +0100

    AppMenu: rework menu key navigation

--- components/AppMenu.jsx
+++ components/AppMenu.jsx
@@ -12,7 +12,6 @@ import {connect} from 'react-redux';
 import classnames from 'classnames';
 import {remove as removeDiacritics} from 'diacritics';
 import isEmpty from 'lodash.isempty';
-import isEqual from 'lodash.isequal';
 import mousetrap from 'mousetrap';
 import PropTypes from 'prop-types';
 
@@ -49,12 +48,11 @@ class AppMenu extends React.Component {
     state = {
         menuVisible: false,
         filter: "",
-        submenusVisible: [],
-        curEntry: null,
-        keyNav: false
+        submenusVisible: []
     };
     constructor(props) {
         super(props);
+        this.menuBtn = null;
         this.menuEl = null;
         this.filterfield = null;
         this.boundShortcuts = [];
@@ -69,9 +67,13 @@ class AppMenu extends React.Component {
         }
     }
     componentDidUpdate(prevProps, prevState) {
-        if (this.state.menuVisible && !prevState.menuVisible && this.filterfield && !this.props.menuCompact) {
+        if (this.state.menuVisible && !prevState.menuVisible && !this.props.menuCompact) {
             // Need to wait until slide in transition is over
-            setTimeout(() => { this.filterfield.focus(); }, 400);
+            setTimeout(() => { this.filterfield?.focus?.(); }, 400);
+            // Delay one cycle
+            setTimeout(() => document.addEventListener('click', this.checkCloseMenu), 0);
+        } else if (prevState.menuVisible && !this.state.menuVisible && !this.props.menuCompact) {
+            document.removeEventListener('click', this.checkCloseMenu);
         }
     }
     componentWillUnmount() {
@@ -79,11 +81,7 @@ class AppMenu extends React.Component {
         if (this.props.appMenuShortcut) {
             mousetrap.unbind(this.props.appMenuShortcut, this.toggleMenu);
         }
-        if (this.state.menuVisible) {
-            document.removeEventListener('click', this.checkCloseMenu);
-            document.removeEventListener('keydown', this.onKeyPress, true);
-            document.removeEventListener('mousemove', this.onMouseMove, true);
-        }
+        document.removeEventListener('click', this.checkCloseMenu);
     }
     addKeyBindings = (items) => {
         items.forEach(item => {
@@ -98,94 +96,12 @@ class AppMenu extends React.Component {
             }
         });
     };
-    onKeyPress = (ev) => {
-        if (ev.key === 'Enter' || ev.key === 'ArrowLeft' || ev.key === 'ArrowUp' || ev.key === 'ArrowRight' || ev.key === 'ArrowDown') {
-            if (!this.state.curEntry) {
-                if (ev.key === 'ArrowUp' || ev.key === 'ArrowDown') {
-                    this.setState({curEntry: [ev.key === 'ArrowUp' ? this.props.menuItems.length - 1 : 0]});
-                }
-            } else {
-                const curEntry = [...this.state.curEntry];
-                const stack = [this.props.menuItems];
-                this.state.curEntry.forEach(entry => {
-                    stack.push(stack[stack.length - 1][entry].subitems);
-                });
-                stack.pop();
-                let leaf = curEntry.pop();
-                const level = stack.length - 1;
-                if (ev.key === 'Enter') {
-                    if (!isEmpty(stack[stack.length - 1][leaf].subitems)) {
-                        this.onSubmenuClicked(stack[stack.length - 1][leaf].key, level);
-                    } else {
-                        this.onMenuitemClicked(stack[stack.length - 1][leaf]);
-                    }
-                } else if (ev.key === 'ArrowLeft') {
-                    if (!isEmpty(stack[stack.length - 1][leaf].subitems) && this.state.submenusVisible[level] === stack[stack.length - 1][leaf].key) {
-                        this.onSubmenuClicked(stack[stack.length - 1][leaf].key, level);
-                    }
-                } else if (ev.key === 'ArrowUp') {
-                    leaf -= 1;
-                    if (leaf >= 0 && !isEmpty(stack[stack.length - 1][leaf].subitems) && this.state.submenusVisible[level] === stack[stack.length - 1][leaf].key) {
-                        curEntry.push(leaf);
-                        leaf = stack[stack.length - 1][leaf].subitems.length - 1;
-                    } else {
-                        while (leaf < 0 && curEntry.length > 0) {
-                            leaf = curEntry.pop();
-                        }
-                        if (leaf < 0) {
-                            leaf = this.props.menuItems.length - 1;
-                        }
-                    }
-                } else if (ev.key === 'ArrowRight') {
-                    if (!isEmpty(stack[stack.length - 1][leaf].subitems) && !this.state.submenusVisible[level]) {
-                        this.onSubmenuClicked(stack[stack.length - 1][leaf].key, level);
-                    }
-                } else if (ev.key === 'ArrowDown') {
-                    if (!isEmpty(stack[stack.length - 1][leaf].subitems) && this.state.submenusVisible[level] === stack[stack.length - 1][leaf].key) {
-                        curEntry.push(leaf);
-                        leaf = 0;
-                    } else {
-                        leaf += 1;
-                        while (leaf > stack[stack.length - 1].length - 1 && curEntry.length > 0) {
-                            leaf = curEntry.pop() + 1;
-                            stack.pop();
-                        }
-                        if (leaf > this.props.menuItems.length - 1) {
-                            leaf = 0;
-                        }
-                    }
-                }
-                this.setState({curEntry: [...curEntry, leaf], keyNav: true});
-            }
-            MiscUtils.killEvent(ev);
-        } else if (ev.key === 'Escape') {
-            this.toggleMenu();
-            MiscUtils.killEvent(ev);
-        }
-    };
-    onMouseMove = (ev) => {
-        if (this.state.keyNav) {
-            this.setState({keyNav: false});
-        }
-        MiscUtils.killEvent(ev);
-    };
     toggleMenu = () => {
         if (!this.state.menuVisible && this.props.currentTaskBlocked) {
             return;
         }
         if (!this.state.menuVisible && this.props.appMenuClearsTask) {
             this.props.setCurrentTask(null);
-        }
-        if (!this.props.keepMenuOpen) {
-            if (!this.state.menuVisible) {
-                document.addEventListener('click', this.checkCloseMenu);
-                document.addEventListener('keydown', this.onKeyPress, true);
-                document.addEventListener('mousemove', this.onMouseMove, true);
-            } else {
-                document.removeEventListener('click', this.checkCloseMenu);
-                document.removeEventListener('keydown', this.onKeyPress, true);
-                document.removeEventListener('mousemove', this.onMouseMove, true);
-            }
         }
         this.props.onMenuToggled(!this.state.menuVisible);
         if (this.props.menuCompact) {
@@ -196,8 +112,8 @@ class AppMenu extends React.Component {
     checkCloseMenu = (ev) => {
         if (this.menuEl && !this.menuEl.contains(ev.target) && !this.props.keepMenuOpen) {
             this.toggleMenu();
+            MiscUtils.killEvent(ev);
         }
-        MiscUtils.killEvent(ev);
     };
     onSubmenuClicked = (key, level) => {
         const a = this.state.submenusVisible[level] === key ? [] : [key];
@@ -214,65 +130,58 @@ class AppMenu extends React.Component {
             this.props.setCurrentTask(item.task || item.key, item.mode, item.mapClickAction || (item.identifyEnabled ? "identify" : null));
         }
     };
-    renderMenuItems = (items, level, filter, path) => {
-        if (items) {
-            return items.map((item, idx) => {
-                const active = isEqual(this.state.curEntry, [...path, idx]);
-                if (item.subitems) {
-                    const subitems = this.renderMenuItems(item.subitems, level + 1, filter, [...path, idx]);
-                    if (filter && isEmpty(subitems)) {
-                        return null;
-                    }
-                    const visible = (filter && !isEmpty(subitems)) || this.state.submenusVisible[level] === item.key;
-                    const className = classnames({
-                        "appmenu-submenu": true,
-                        "appmenu-submenu-active": active,
-                        "appmenu-submenu-expanded": visible
-                    });
-                    return (
-                        <li className={className} key={item.key ?? item.title}
-                            onClick={() => this.onSubmenuClicked(item.key, level)}
-                            onMouseEnter={() => { if (!this.state.keyNav) { this.setState({curEntry: [...path, idx]}); } } }
-                            onMouseLeave={() => { if (!this.state.keyNav) { this.setState({curEntry: null}); } } }
-                            ref={el => { if (active && el && this.state.keyNav) { el.scrollIntoView(false); } }}
-                        >
-                            <Icon icon={item.icon} size="xlarge"/>
-                            {item.title ? LocaleUtils.tr(item.title) : LocaleUtils.tr("appmenu.items." + item.key)}
-                            <ul>
-                                {subitems}
-                            </ul>
-                        </li>
-                    );
-                } else {
-                    const trargs = item.trargs || [];
-                    const label = item.title ? LocaleUtils.tr(item.title, ...trargs) : LocaleUtils.tr("appmenu.items." + item.key + (item.mode || ""), ...trargs);
-                    const comment = item.comment ? LocaleUtils.tr("appmenu.items." + item.key + (item.mode || "") + "_comment", ...trargs) : "";
-                    if (!filter || removeDiacritics(label.toLowerCase()).match(filter) || (comment && removeDiacritics(comment.toLowerCase()).match(filter))) {
-                        const className = classnames({
-                            "appmenu-leaf": true,
-                            "appmenu-leaf-active": active
-                        });
-                        return (
-                            <li className={className} key={item.key ? item.key + (item.mode || "") : item.title}
-                                onClick={() => this.onMenuitemClicked(item)}
-                                onMouseEnter={() => { if (!this.state.keyNav) { this.setState({curEntry: [...path, idx]}); } } }
-                                onMouseLeave={() => { if (!this.state.keyNav) { this.setState({curEntry: null}); } } }
-                                ref={el => { if (active && el && this.state.keyNav) { el.scrollIntoView(false); } }}
-                            >
-                                <Icon icon={item.icon} size="xlarge"/>
-                                <span className="appmenu-leaf-label">
-                                    {label}
-                                    {comment ? (<div className="appmenu-leaf-comment">{comment}</div>) : null}
-                                </span>
-                            </li>
-                        );
-                    }
+    renderMenuItems = (items, level, filter, submenu = false) => {
+        return (items || []).map(item => {
+            if (item.subitems) {
+                const expanded = filter || this.state.submenusVisible[level] === item.key;
+                const subitems = expanded ? this.renderMenuItems(item.subitems, level + 1, filter, true) : null;
+                if (filter && isEmpty(subitems)) {
                     return null;
                 }
-            }).filter(x => x);
-        } else {
-            return null;
-        }
+                const className = classnames({
+                    "appmenu-menu-item": true,
+                    "appmenu-submenu": true,
+                    "appmenu-submenu-expanded": expanded
+                });
+                return [(
+                    <div className={className} key={item.key ?? item.title}
+                        onClick={() => this.onSubmenuClicked(item.key, level)}
+                        onKeyDown={this.keyNav}
+                        onMouseOver={ev => ev.target.focus()}
+                        tabIndex={0}
+                    >
+                        <Icon icon={item.icon} size="xlarge"/>
+                        {item.title ? LocaleUtils.tr(item.title) : LocaleUtils.tr("appmenu.items." + item.key)}
+                    </div>
+                ),
+                subitems];
+            } else {
+                const trargs = item.trargs || [];
+                const label = item.title ? LocaleUtils.tr(item.title, ...trargs) : LocaleUtils.tr("appmenu.items." + item.key + (item.mode || ""), ...trargs);
+                const comment = item.comment ? LocaleUtils.tr("appmenu.items." + item.key + (item.mode || "") + "_comment", ...trargs) : "";
+                if (!filter || removeDiacritics(label.toLowerCase()).match(filter) || (comment && removeDiacritics(comment.toLowerCase()).match(filter))) {
+                    const className = classnames({
+                        "appmenu-menu-item": true,
+                        "appmenu-menu-item-nested": submenu
+                    });
+                    return (
+                        <div className={className} key={item.key ? item.key + (item.mode || "") : item.title}
+                            onClick={() => this.onMenuitemClicked(item)}
+                            onKeyDown={this.keyNav}
+                            onMouseOver={ev => ev.target.focus()}
+                            tabIndex={0}
+                        >
+                            <Icon icon={item.icon} size="xlarge"/>
+                            <span className="appmenu-menu-item-label">
+                                {label}
+                                {comment ? (<div className="appmenu-menu-item-comment">{comment}</div>) : null}
+                            </span>
+                        </div>
+                    );
+                }
+                return null;
+            }
+        });
     };
     render() {
         const isMobile = ConfigUtils.isMobile();
@@ -287,20 +196,28 @@ class AppMenu extends React.Component {
         });
         const filter = this.state.filter ? new RegExp(removeDiacritics(this.state.filter).replace(/[-[\]/{}()*+?.\\^$|]/g, "\\$&"), "i") : null;
         return (
-            <div className={className} ref={el => { this.menuEl = el; MiscUtils.setupKillTouchEvents(el); }}
-            >
-                <div className="appmenu-button" onClick={this.toggleMenu} onKeyDown={MiscUtils.checkKeyActivate} tabIndex={0} title={this.props.buttonLabel}>
+            <div className={className}>
+                <div
+                    className="appmenu-button" onClick={this.toggleMenu} onKeyDown={this.btnKeyNav}
+                    ref={el => { this.menuBtn = el; }} tabIndex={0} title={this.props.buttonLabel}
+                >
                     {showLabel ? (<span className="appmenu-label">{this.props.buttonLabel}</span>) : null}
                     <span className="appmenu-icon">
                         <Icon icon="menu-hamburger"/>
                     </span>
                 </div>
                 <div className="appmenu-menu-container" tabIndex={-1}>
-                    <ul className="appmenu-menu" inert={visible ? undefined : "true"}>
+                    <div className="appmenu-menu" inert={visible ? undefined : "true"} onMouseLeave={this.clearFocus} ref={el => { this.menuEl = el; MiscUtils.setupKillTouchEvents(el); }}>
                         {this.props.showFilterField ? (
-                            <li className="appmenu-leaf">
+                            <div
+                                className="appmenu-menu-item appmenu-menu-item-filter"
+                                onFocus={this.focusFilterField}
+                                onKeyDown={this.keyNav}
+                                onMouseOver={ev => ev.target.focus()}
+                                tabIndex={0}
+                            >
                                 <Icon icon={"search"} size="xlarge"/>
-                                <InputContainer className="appmenu-filter">
+                                <InputContainer>
                                     <input onChange={ev => this.setState({filter: ev.target.value, curEntry: null})}
                                         placeholder={LocaleUtils.tr("appmenu.filter")} ref={this.setFilterField}
                                         role="input"
@@ -308,10 +225,10 @@ class AppMenu extends React.Component {
                                         value={this.state.filter}/>
                                     <Icon icon="clear" onClick={() => this.setState({filter: ""})} role="suffix" />
                                 </InputContainer>
-                            </li>
+                            </div>
                         ) : null}
-                        {this.renderMenuItems(this.props.menuItems, 0, filter, [])}
-                    </ul>
+                        {this.renderMenuItems(this.props.menuItems, 0, filter)}
+                    </div>
                 </div>
             </div>
         );
@@ -320,6 +237,47 @@ class AppMenu extends React.Component {
         this.filterfield = el;
         if (this.props.appMenuShortcut) {
             mousetrap(el).bind(this.props.appMenuShortcut, this.toggleMenu);
+        }
+    };
+    focusFilterField = (ev) => {
+        if (ev.target.classList.contains("appmenu-menu-item-filter")) {
+            this.filterfield?.focus?.();
+        }
+    };
+    clearFocus = () => {
+        document.activeElement.blur();
+    };
+    btnKeyNav = (ev) => {
+        if (ev.key === 'ArrowDown') {
+            this.menuEl.children[0]?.focus();
+        } else if (ev.key === 'ArrowUp') {
+            this.menuEl.children[this.menuEl.children.length - 1]?.focus();
+        } else {
+            MiscUtils.checkKeyActivate(ev);
+        }
+    };
+    keyNav = (ev) => {
+        if (ev.key === 'ArrowDown' || ev.key === 'ArrowUp') {
+            const childCount = this.menuEl.children.length;
+            const delta = ev.key === 'ArrowUp' ? -1 : 1;
+            let currentIndex = Array.from(this.menuEl.children).findIndex(el => document.activeElement === el || el.contains(document.activeElement));
+            if (currentIndex === -1) {
+                currentIndex = delta === 1 ? childCount - 1 : 0;
+            }
+            const next = (currentIndex + childCount + delta) % childCount;
+            this.menuEl.children[next].focus();
+            MiscUtils.killEvent(ev);
+        } else if (ev.key === 'Escape') {
+            if (!this.props.menuCompact) {
+                this.toggleMenu();
+            }
+            this.menuBtn?.focus?.();
+            MiscUtils.killEvent(ev);
+        } else if (ev.key === 'Enter' || ev.key === ' ') {
+            if (ev.target.classList.contains("appmenu-menu-item")) {
+                ev.target.click();
+                MiscUtils.killEvent(ev);
+            }
         }
     };
 }
