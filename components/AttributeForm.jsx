cb1dcb1eb5949e8d556e0a7d4a1270430f26ea8d
AmandaZwicky <amanda.zwicky@rsrg.com>
2026-02-20 10:00:46 +0100

    use more specific class names

--- components/AttributeForm.jsx
+++ components/AttributeForm.jsx
@@ -147,11 +147,11 @@ class AttributeForm extends React.Component {
             readOnlyMsg = LocaleUtils.tr("editing.geomnonzeroz");
         }
         return (
-            <div className={`AttributeForm ${this.state.childEdit ? "has-child" : ""}`}>
+            <div className={`AttributeForm ${this.state.childEdit ? "attrib-form-has-child" : ""}`}>
                 {readOnlyMsg ? (
                     <div className="attrib-form-geom-readonly">{readOnlyMsg}</div>
                 ) : null}
-                <form className="parent-form" action="" onChange={ev => this.formChanged(ev)} onSubmit={this.onSubmit} ref={this.setupChangedObserver}>
+                <form className="attrib-form-parent-form" action="" onChange={ev => this.formChanged(ev)} onSubmit={this.onSubmit} ref={this.setupChangedObserver}>
                     {editConfig.form ? (
                         <QtDesignerForm addRelationRecord={this.addRelationRecord} editConfig={editConfig}
                             editRelationRecord={this.editRelationRecord}
