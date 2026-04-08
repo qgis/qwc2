ec2a03a1b17551b5bf4f99f3eb99dcedd9268f02
lamptorne <lamptorne@localhost-live.home>
2026-04-08 09:58:19 +0200

    fix css

--- components/widgets/LayerCatalogWidget.jsx
+++ components/widgets/LayerCatalogWidget.jsx
@@ -73,7 +73,7 @@ class LayerCatalogWidget extends React.PureComponent {
                     {hasSublayers && type === "wms" ? (
                         <Icon icon="group" onClick={() => this.checkAddServiceLayer(entry, true)} title={LocaleUtils.tr("importlayer.asgroup")} />
                     ) : null}
-                    {entry.link ? <a className="layer-catalog-widget-entry-link" title={LocaleUtils.tr("layercatalog.openlink")}> <Icon icon="info-sign" onClick={ev => this.openUrl(ev, entry.link, entry.target)} /> </a> : null}
+                    {entry.link ? <Icon className="layer-catalog-widget-entry-link" icon="info-sign"  onClick={ev => this.openUrl(ev, entry.link, entry.target)} title={LocaleUtils.tr("layercatalog.openlink")} /> : null}
                 </div>
                 {entry.expanded ? sublayers : null}
             </div>
