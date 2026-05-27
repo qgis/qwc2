f95e80f18d69f56f978b1db8c8e022ded82322ba
CebullaD <daniel.cebulla@jena.de>
2026-05-27 17:36:46 +0200

    2D BottomBar functionality also in 3D BottomBar

--- plugins/map3d/BottomBar3D.jsx
+++ plugins/map3d/BottomBar3D.jsx
@@ -11,8 +11,9 @@ import {connect} from 'react-redux';
 
 import PropTypes from 'prop-types';
 
-import {setBottombarHeight} from '../../actions/windows';
+import {openExternalUrl, setBottombarHeight} from '../../actions/windows';
 import CoordinatesUtils from '../../utils/CoordinatesUtils';
+import LocaleUtils from '../../utils/LocaleUtils';
 
 import './style/BottomBar3D.css';
 
@@ -22,8 +23,36 @@ import './style/BottomBar3D.css';
  */
 class BottomBar3D extends React.Component {
     static propTypes = {
+        /** Additional bottombar links.`side` can be `left` or `right` (default). */
+        additionalBottomBarLinks: PropTypes.arrayOf(PropTypes.shape({
+            label: PropTypes.string,
+            labelMsgId: PropTypes.string,
+            side: PropTypes.string,
+            url: PropTypes.string,
+            urlTarget: PropTypes.string,
+            icon: PropTypes.string
+        })),
+        /** Whether to display the coordinates in the bottom bar. */
+        displayCoordinates: PropTypes.bool,
+        fullscreen: PropTypes.bool,
+        openExternalUrl: PropTypes.func,
         sceneContext: PropTypes.object,
-        setBottombarHeight: PropTypes.func
+        setBottombarHeight: PropTypes.func,
+        /** The URL of the terms label anchor. */
+        termsUrl: PropTypes.string,
+        /** Icon of the terms inline window. Relevant only when `termsUrlTarget` is `iframe`. */
+        termsUrlIcon: PropTypes.string,
+        /** The target where to open the terms URL. If `iframe`, it will be displayed in an inline window, otherwise in a new tab. You can also use the `:iframedialog:<dialogname>:<options>` syntax to set up the inline window. */
+        termsUrlTarget: PropTypes.string,
+        /** The URL of the viewer title label anchor. */
+        viewertitleUrl: PropTypes.string,
+        /** Icon of the viewer title inline window. Relevant only when `viewertitleUrl` is `iframe`. */
+        viewertitleUrlIcon: PropTypes.string,
+        /** The target where to open the viewer title URL. If `iframe`, it will be displayed in an inline window, otherwise in a new tab. You can also use the `:iframedialog:<dialogname>:<options>` syntax to set up the inline window. */
+        viewertitleUrlTarget: PropTypes.string
+    };
+    static defaultProps = {
+        displayCoordinates: true
     };
     state = {
         cursorPosition: null,
@@ -39,23 +68,64 @@ class BottomBar3D extends React.Component {
         clearTimeout(this.cursorPositionTimeout);
     }
     render() {
+        if (this.props.fullscreen) {
+            return null;
+        }
+        const leftBottomLinks = (this.props.additionalBottomBarLinks || []).filter(entry => entry.side === "left").map(this.renderLink);
+        const rightBottomLinks = (this.props.additionalBottomBarLinks || []).filter(entry => entry.side !== "left").map(this.renderLink); 
+        if (this.props.viewertitleUrl) {
+            const entry = {url: this.props.viewertitleUrl, urlTarget: this.props.viewertitleUrlTarget, label: LocaleUtils.tr("bottombar.viewertitle_label"), icon: this.props.viewertitleUrlIcon};
+            rightBottomLinks.push(this.renderLink(entry));
+        }
+        if (this.props.termsUrl) {
+            const entry = {url: this.props.termsUrl, urlTarget: this.props.termsUrlTarget, label: LocaleUtils.tr("bottombar.terms_label"), icon: this.props.termsUrlIcon};
+            rightBottomLinks.push(this.renderLink(entry));
+        }
+        let coordinates = null;
+        if (this.props.displayCoordinates) {
+            coordinates = (
+                <div className="controlgroup">
+                    <div className="map3d-bottombar-position">
+                        {(this.state.cursorPosition || []).map(x => x.toFixed(0)).join(" ")}
+                    </div>
+                    <div className="map3d-bottombar-projection">
+                        {this.props.sceneContext.mapCrs ? CoordinatesUtils.getAvailableCRS()[this.props.sceneContext.mapCrs].label : ""}
+                    </div>
+                    <div className="map3d-bottombar-spacer" />
+                </div>
+            );
+        }
         return (
             <div className="map3d-bottombar" ref={this.storeHeight}>
                 <div className="map3d-bottombar-progress">
                     <div className="map3d-bottombar-progressbar" style={{width: this.state.progress}} />
                     <div className="map3d-bottombar-progress-label">{this.state.progress}</div>
                 </div>
+                <span className="bottombar-links">
+                    {leftBottomLinks}
+                </span>
+                {coordinates}
                 <div className="map3d-bottombar-spacer" />
-                <div className="map3d-bottombar-position">
-                    {(this.state.cursorPosition || []).map(x => x.toFixed(0)).join(" ")}
-                </div>
-                <div className="map3d-bottombar-projection">
-                    {this.props.sceneContext.mapCrs ? CoordinatesUtils.getAvailableCRS()[this.props.sceneContext.mapCrs].label : ""}
-                </div>
-                <div className="map3d-bottombar-spacer" />
+                <span className="bottombar-links">
+                    {rightBottomLinks}
+                </span>
             </div>
         );
     }
+    renderLink = (entry) => {
+        return (
+            <a href={entry.url} key={entry.labelMsgId ?? entry.label} onClick={(ev) => this.openUrl(ev, entry.url, entry.urlTarget, entry.labelMsgId ? LocaleUtils.tr(entry.labelMsgId) : entry.label, entry.icon)}>
+                <span className="extra_label">{entry.labelMsgId ? LocaleUtils.tr(entry.labelMsgId) : entry.label}</span>
+            </a>
+        );
+    };
+    openUrl = (ev, url, target, title, icon) => {
+        if (target === "iframe") {
+            target = ":iframedialog:externallinkiframe";
+        }
+        this.props.openExternalUrl(url, target, {title, icon});
+        ev.preventDefault();
+    };
     scheduleGetCursorPosition = (ev) => {
         const rect = ev.currentTarget.getBoundingClientRect();
         const x = (ev.clientX - rect.left) / rect.width * 2 - 1;
@@ -77,6 +147,9 @@ class BottomBar3D extends React.Component {
     };
 }
 
-export default connect(() => ({}), {
-    setBottombarHeight: setBottombarHeight
+export default connect((state) => ({
+    fullscreen: state.display?.fullscreen,
+}), {
+    openExternalUrl: openExternalUrl,
+    setBottombarHeight: setBottombarHeight,
 })(BottomBar3D);
