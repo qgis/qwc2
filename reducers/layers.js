e5bd95629a0f3d5c137c2e829178045463afe146
Sandro Mani <manisandro@gmail.com>
2026-06-16 15:02:30 +0200

    reducers/layers: ensure removed layer is cleared from loading state

--- reducers/layers.js
+++ reducers/layers.js
@@ -223,6 +223,7 @@ export default function layers(state = defaultState, action) {
         }
         let newLayers = state.flat;
         let newEditConfigs = state.editConfigs;
+        let newLoading = state.loading;
         if (layer.role === LayerRole.BACKGROUND || isEmpty(action.sublayerpath)) {
             const position = state.flat.findIndex(l => l.id === action.layerId);
             newLayers = [...newLayers];
@@ -239,6 +240,7 @@ export default function layers(state = defaultState, action) {
                 newEditConfigs = {...newEditConfigs};
                 delete newEditConfigs[layer.wms_name];
             }
+            newLoading = [...state.loading].filter(layerId => layerId !== action.layerId);
         } else {
             const sublayer = LayerUtils.getSublayer(layer, action.sublayerpath);
             const sublayernames = LayerUtils.getSublayerNames(layer);
@@ -257,7 +259,7 @@ export default function layers(state = defaultState, action) {
             });
         }
         UrlParams.updateParams({l: LayerUtils.buildWMSLayerUrlParam(newLayers)});
-        return {...state, flat: newLayers, editConfigs: newEditConfigs};
+        return {...state, flat: newLayers, editConfigs: newEditConfigs, loading: newLoading};
     }
     case ADD_LAYER_FEATURES: {
         const layerId = action.layer.id || uuidv4();
